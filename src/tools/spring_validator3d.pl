8bea859f5f9ef31ca87337c56a8eb628fe7e7ed7
Boyce Griffith <boyceg@gmail.com>
2008-09-09 00:31:07 +0000

    added spring and beam validation tools

--- src/tools/spring_validator3d.pl
+++ src/tools/spring_validator3d.pl
@@ -1,27 +1,40 @@
 #!/usr/bin/perl -w
 #
-# filename: check_spring_rest_length.pl
+# filename: spring_validator3d.pl
 # author: Boyce Griffith
-# usage: check_spring_rest_length.pl <input filename> <stiffness> <output filename>
+# usage: spring_validator3d.pl <input filename>
 #
-# A simple Perl script to determine whether the rest lengths of the
-# springs in an IBAMR spring file are consistent with the initial
-# lengths in the corresponding IBAMR vertex file.
+# A simple Perl script to determine whether the spring specifications
+# in an IBAMR spring file are valid, and whether the specified spring
+# resting lengths are consistent with the initial displacements in the
+# corresponding IBAMR vertex file.
+#
+# NOTE: This utility will generate a large number of warning messages
+# for sturctures where the initial lengths and resting lengths differ.
 
 use List::Util qw[min max];
 
 if ($#ARGV != 0) {
-    die "incorrect number of command line arguments.\nusage:\n  check_spring_rest_length.pl <input filename>\n";
+    die "incorrect number of command line arguments.\nusage:\n  spring_validator3d.pl <input filename>\n";
 }
+
+# boolean controling whether to check to see if the initial
+# displacement equals the resting length.
+$check_for_consistent_rest_lengths = 1;  # 1 = enabled; 0 = disabled
+$eps = 1.0e-8;
+
+print "\n";
+if ($check_for_consistent_rest_lengths) {
+    print "checks for consistent spring rest lengths are ENABLED!\n";
+    print "warning messages WILL be printed if the initial displacements and resting lengths differ by more than a relative error tolerance of $eps.\n";
+} else {
+    print "checks for consistent spring rest lengths are DISABLED!\n";
+    print "warning messages WILL NOT be printed if the initial displacements and resting lengths differ.\n";
+}
+print "\n";
 
 # parse the command line arguments
 $input_filename = shift @ARGV;  chomp $input_filename;
-
-print "input file: $input_filename\n";
-
-# open the input files
-open(VERTEX_IN, "$input_filename.vertex") || die "error: cannot open $input_filename.vertex for reading: $!";
-open(SPRING_IN, "$input_filename.spring") || die "error: cannot open $input_filename.spring for reading: $!";
 
 # the first line in the vertex input file has the format:
 #
@@ -32,17 +45,34 @@ open(SPRING_IN, "$input_filename.spring") || die "error: cannot open $input_file
 #   <x> <y> <z> (comments)
 #
 # where items in ()'s are optional
-<VERTEX_IN>;
+open(VERTEX_IN, "$input_filename.vertex") || die "error: cannot open $input_filename.vertex for reading: $!";
+$_ = <VERTEX_IN>;
+chomp;
+@line = split;
+$num_vertices_expected = $line[0];
 $i = 0;
+$num_vertices = 0;
+$line_number = 1;
 while (<VERTEX_IN>) {
     chomp;
+    @line = split('#');
+    $_ = $line[0];
     @line = split;
+
     $x[$i] = $line[0];
     $y[$i] = $line[1];
     $z[$i] = $line[2];
+
     $i += 1;
+    $num_vertices += 1;
+    $line_number += 1;
 }
-print "read $i vertices\n";
+print "read $num_vertices vertices from vertex file: $input_filename.vertex\n\n";
+if ($num_vertices != $num_vertices_expected) {
+    print "WARNING: expected to read: $num_vertices_expected from vertex file $input_filename.vertex\n";
+    print "         actually read:    $num_vertices\n\n";
+}
+close(VERTEX_IN);
 
 # the first line in the spring input file has the format:
 #
@@ -53,25 +83,68 @@ print "read $i vertices\n";
 #   <first node> <second node> <stiffness> <rest length> (force fcn index) (comments)
 #
 # where items in ()'s are optional
-<SPRING_IN>;
+open(SPRING_IN, "$input_filename.spring") || die "error: cannot open $input_filename.spring for reading: $!";
+$_ = <SPRING_IN>;
+chomp;
+@line = split;
+$num_springs_expected = $line[0];
 $i = 0;
+$num_springs = 0;
+$line_number = 1;
 while (<SPRING_IN>) {
     chomp;
+    @line = split('#');
+    $_ = $line[0];
     @line = split;
+
     $idx0 = $line[0];
     $idx1 = $line[1];
+    $stf  = $line[2];
     $rst  = $line[3];
 
-    $dx[0] = $x[$idx0] - $x[$idx1];
-    $dx[1] = $y[$idx0] - $y[$idx1];
-    $dx[2] = $z[$idx0] - $z[$idx1];
-
-    $rst_actual = sqrt($dx[0]*$dx[0]+$dx[1]*$dx[1]+$dx[2]*$dx[2]);
-    if (abs($rst_actual - $rst)/max($rst_actual,$rst,1.0) > 1.0e-6) {
-        print "check spring on line: $i\n";
-        print "rest length in spring file = $rst\n";
-        print "initial displacement in vertex file = $rst_actual\n";
+    if ($idx0 < 0) {
+	print "ERROR: index 0 on line $line_number in spring file $input_filename.spring is negative\n";
     }
+
+    if ($idx1 < 0) {
+	print "ERROR: index 1 on line $line_number in spring file $input_filename.spring is negative\n";
+    }
+
+    if ($idx0 >= $num_vertices) {
+	print "ERROR: index 0 on line $line_number in spring file $input_filename.spring is greater than or equal to the number of vertices in vertex file $input_filename.vertex\n";
+    }
+
+    if ($idx1 >= $num_vertices) {
+	print "ERROR: index 1 on line $line_number in spring file $input_filename.spring is greater than or equal to the number of vertices in vertex file $input_filename.vertex\n";
+    }
+
+    if ($stf < 0.0) {
+	print "ERROR: spring constant on line $line_number in spring file $input_filename.spring is negative\n";
+    }
+
+    if ($rst < 0.0) {
+	print "ERROR: rest length on line $line_number in spring file $input_filename.spring is negative\n";
+    }
+
+    if ($check_for_consistent_rest_lengths) {
+	$dx[0] = $x[$idx0] - $x[$idx1];
+	$dx[1] = $y[$idx0] - $y[$idx1];
+	$dx[2] = $z[$idx0] - $z[$idx1];
+	$rst_actual = sqrt($dx[0]*$dx[0]+$dx[1]*$dx[1]+$dx[2]*$dx[2]);
+	if (abs($rst_actual - $rst)/max($rst_actual,$rst,1.0) > $eps) {
+	    print "WARNING: rest length on line $line_number in spring file $input_filename.spring is different from initial displacement in vertex file $input_filename.vertex\n";
+	    print "         rest length in spring file $input_filename.spring: $rst\n";
+	    print "         initial displacement in vertex file $input_filename.vertex: $rst_actual\n";
+	}
+    }
+
     $i += 1;
+    $num_springs += 1;
+    $line_number += 1;
 }
-print "read $i springs\n";
+print "read $num_springs springs from spring file: $input_filename.spring\n";
+if ($num_springs != $num_springs_expected) {
+    print "WARNING: expected to read: $num_springs_expected springs from spring file $input_filename.spring\n";
+    print "         actually read:    $num_springs springs\n";
+}
+close(SPRING_IN);
