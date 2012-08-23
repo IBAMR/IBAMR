4ab4543ab55533d77a62f6c065630d514bb4d0ea
Boyce Griffith <boyceg@gmail.com>
2012-08-23 21:20:34 +0000

    branch boyceg: updating error messages for dt=0

--- ibtk/src/utilities/HierarchyIntegrator.C
+++ ibtk/src/utilities/HierarchyIntegrator.C
@@ -248,22 +248,22 @@ HierarchyIntegrator::advanceHierarchy(
 {
     if (d_integrator_time+dt > d_end_time)
     {
-        pout << "WARNING: reducing dt so that current_time+dt = end_time.\n";
+        pout << "WARNING: at time = " << d_integrator_time << " reducing dt so that current_time+dt = end_time.\n";
         dt = d_end_time-d_integrator_time;
     }
     const double current_time = d_integrator_time;
     const double new_time = d_integrator_time+dt;
     if (dt < 0.0)
     {
-        TBOX_ERROR(d_object_name << "::advanceHierarchy(): time step size dt is negative.\n");
+        TBOX_ERROR(d_object_name << "::advanceHierarchy(): at time = " << d_integrator_time << " time step size dt is negative.\n");
     }
     else if (dt == 0.0)
     {
-        TBOX_ERROR(d_object_name << "::advanceHierarchy(): time step size dt is zero.\n");
+        TBOX_ERROR(d_object_name << "::advanceHierarchy(): at time = " << d_integrator_time << " time step size dt is zero.\n");
     }
     else if (current_time == new_time || MathUtilities<double>::equalEps(current_time,new_time))
     {
-        TBOX_ERROR(d_object_name << "::advanceHierarchy(): time step size dt is zero to machine precision.\n");
+        TBOX_ERROR(d_object_name << "::advanceHierarchy(): at time = " << d_integrator_time << " time step size dt is zero to machine precision.\n");
     }
     if (d_enable_logging) plog << d_object_name << "::advanceHierarchy(): time interval = [" << current_time << "," << new_time << "], dt = " << dt << "\n";
 
