86160a2e59a8cf6c8269a1a0160cc575fd23cd4e
Boyce Griffith <boyceg@gmail.com>
2012-09-05 20:41:14 +0000

    branch boyceg: adding stochastic R-T example (parameters not yet tuned)

---
+++ examples/navier_stokes/ex6/BoussinesqForcing.C
@@ -0,0 +1,148 @@
+// Filename: BoussinesqForcing.C
+// Created on 24 Aug 2012 by Boyce Griffith
+//
+// Copyright (c) 2002-2010, Boyce Griffith
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice,
+//      this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of New York University nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include "BoussinesqForcing.h"
+
+/////////////////////////////// INCLUDES /////////////////////////////////////
+
+#ifndef included_IBAMR_config
+#include <IBAMR_config.h>
+#define included_IBAMR_config
+#endif
+
+#ifndef included_SAMRAI_config
+#include <SAMRAI_config.h>
+#define included_SAMRAI_config
+#endif
+
+// SAMRAI INCLUDES
+#include <HierarchyDataOpsManager.h>
+
+/////////////////////////////// STATIC ///////////////////////////////////////
+
+/////////////////////////////// PUBLIC ///////////////////////////////////////
+
+BoussinesqForcing::BoussinesqForcing(
+    Pointer<Variable<NDIM> > T_var,
+    Pointer<AdvDiffHierarchyIntegrator> adv_diff_hier_integrator,
+    int gamma)
+    : d_T_var(T_var),
+      d_adv_diff_hier_integrator(adv_diff_hier_integrator),
+      d_gamma(gamma)
+{
+    // intentionally blank
+    return;
+}// BoussinesqForcing
+
+BoussinesqForcing::~BoussinesqForcing()
+{
+    // intentionally blank
+    return;
+}// ~BoussinesqForcing
+
+bool
+BoussinesqForcing::isTimeDependent() const
+{
+    return true;
+}// isTimeDependent
+
+void
+BoussinesqForcing::setDataOnPatchHierarchy(
+    const int data_idx,
+    Pointer<Variable<NDIM> > var,
+    Pointer<PatchHierarchy<NDIM> > hierarchy,
+    const double data_time,
+    const bool initial_time,
+    const int coarsest_ln_in,
+    const int finest_ln_in)
+{
+    // Communicate ghost-cell data.
+    if (!initial_time)
+    {
+        VariableDatabase<NDIM>* var_db = VariableDatabase<NDIM>::getDatabase();
+        int T_current_idx = var_db->mapVariableAndContextToIndex(d_T_var, d_adv_diff_hier_integrator->getCurrentContext());
+        int T_scratch_idx = var_db->mapVariableAndContextToIndex(d_T_var, d_adv_diff_hier_integrator->getScratchContext());
+        int T_new_idx = var_db->mapVariableAndContextToIndex(d_T_var, d_adv_diff_hier_integrator->getNewContext());
+        HierarchyDataOpsManager<NDIM>* hier_data_ops_manager = HierarchyDataOpsManager<NDIM>::getManager();
+        Pointer<HierarchyDataOpsReal<NDIM,double> > hier_cc_data_ops = hier_data_ops_manager->getOperationsDouble(d_T_var, hierarchy, /*get_unique*/ true);
+        if (d_adv_diff_hier_integrator->getCurrentCycleNumber() == 0)
+        {
+            hier_cc_data_ops->copyData(T_scratch_idx, T_current_idx);
+        }
+        else
+        {
+#ifdef DEBUG_CHECK_ASSERTIONS
+            TBOX_ASSERT(d_adv_diff_hier_integrator->getCurrentCycleNumber() > 0);
+#endif
+            hier_cc_data_ops->linearSum(T_scratch_idx, 0.5, T_current_idx, 0.5, T_new_idx);
+        }
+        typedef HierarchyGhostCellInterpolation::InterpolationTransactionComponent InterpolationTransactionComponent;
+        InterpolationTransactionComponent ghost_fill_component(T_scratch_idx, "CONSERVATIVE_COARSEN", "LINEAR", false, d_adv_diff_hier_integrator->getPhysicalBcCoefs(d_T_var));
+        HierarchyGhostCellInterpolation ghost_fill_op;
+        ghost_fill_op.initializeOperatorState(ghost_fill_component, hierarchy);
+        ghost_fill_op.fillData(data_time);
+    }
+
+    // Compute a staggered-grid approximation to -gamma*T on each patch level.
+    const int coarsest_ln = (coarsest_ln_in == -1 ? 0 : coarsest_ln_in);
+    const int finest_ln = (finest_ln_in == -1 ? hierarchy->getFinestLevelNumber() : finest_ln_in);
+    for (int level_num = coarsest_ln; level_num <= finest_ln; ++level_num)
+    {
+        setDataOnPatchLevel(data_idx, var, hierarchy->getPatchLevel(level_num), data_time, initial_time);
+    }
+    return;
+}// setDataOnPatchHierarchy
+
+void
+BoussinesqForcing::setDataOnPatch(
+    const int data_idx,
+    Pointer<Variable<NDIM> > /*var*/,
+    Pointer<Patch<NDIM> > patch,
+    const double /*data_time*/,
+    const bool initial_time,
+    Pointer<PatchLevel<NDIM> > /*patch_level*/)
+{
+    Pointer<SideData<NDIM,double> > F_data = patch->getPatchData(data_idx);
+    F_data->fillAll(0.0);
+    if (initial_time) return;
+    Pointer<CellData<NDIM,double> > T_scratch_data = patch->getPatchData(d_T_var, d_adv_diff_hier_integrator->getScratchContext());
+    const Box<NDIM>& patch_box = patch->getBox();
+    const int axis = NDIM-1;
+    for (Box<NDIM>::Iterator it(SideGeometry<NDIM>::toSideBox(patch_box,axis)); it; it++)
+    {
+        SideIndex<NDIM> s_i(it(),axis,0);
+        (*F_data)(s_i) = -d_gamma*0.5*((*T_scratch_data)(s_i.toCell(1))+(*T_scratch_data)(s_i.toCell(0)));
+    }
+    return;
+}// setDataOnPatch
+
+//////////////////////////////////////////////////////////////////////////////
