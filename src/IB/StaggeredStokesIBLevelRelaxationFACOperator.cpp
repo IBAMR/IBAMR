246f433569448802fe0a84e2fc556a7e88a9ac89
amneetb <mail2amneet@gmail.com>
2015-09-18 17:25:02 -0400

    Fixing prefix for level solvers in StaggeredStokesIBRelaxationFACOperator. PETSc command line preference is first level_N_ and then it reverts to finding level_.

--- src/IB/StaggeredStokesIBLevelRelaxationFACOperator.cpp
+++ src/IB/StaggeredStokesIBLevelRelaxationFACOperator.cpp
@@ -939,7 +939,7 @@ void StaggeredStokesIBLevelRelaxationFACOperator::initializeOperatorState(
         if (!level_solver)
         {
             std::ostringstream level_solver_stream;
-            level_solver_stream << d_level_solver_default_options_prefix;
+            level_solver_stream << d_level_solver_default_options_prefix << ln << "_";
             level_solver = StaggeredStokesSolverManager::getManager()->allocateSolver(
                 d_level_solver_type, d_object_name + "::level_solver", d_level_solver_db, level_solver_stream.str());
         }
