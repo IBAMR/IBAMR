9300f928d116a502ae0fa3301cbff9433377626d
Matthew G. Knepley <knepley@gmail.com>
2016-01-08 06:24:54 -0600

    Configure: Fixed Eigen include directory

--- config/IBAMR/packages/eigen.py
+++ config/IBAMR/packages/eigen.py
@@ -3,10 +3,11 @@ import config.package
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.download  = ['http://bitbucket.org/eigen/eigen/get/3.2.2.tar.bz2']
-    self.functions = []
-    self.includes  = ['eigen3/Eigen/Core']
-    self.liblist   = [[]]
-    self.pkgname   = 'eigen-3.2.2'
-    self.cxx       = 1
+    self.download   = ['http://bitbucket.org/eigen/eigen/get/3.2.2.tar.bz2']
+    self.functions  = []
+    self.includes   = ['Eigen/Core']
+    self.includedir = 'include/eigen3'
+    self.liblist    = [[]]
+    self.pkgname    = 'eigen-3.2.2'
+    self.cxx        = 1
     return
