5c52f3ef63241853eee369fe800442f6bf0e70d9
Boyce Griffith <boyceg@gmail.com>
2016-01-24 22:45:46 -0500

    updating non-bonded interaction force class files to standard IBAMR format

--- src/IB/Wall.cpp
+++ src/IB/Wall.cpp
@@ -1,17 +1,56 @@
+// Filename: Wall.cpp
+// Created by Steven DeLong
+//
+// Copyright (c) 2002-2015, Steven DeLong and Boyce Griffith
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice,
+//      this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of The University of North Carolina nor the names of
+//      its contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
-#include "Wall.h"
+/////////////////////////////// INCLUDES /////////////////////////////////////
 
-////////////////////////////////// Wall functions  //////////////////////////////////////////
-    
-Wall::Wall(Pointer<Database> wall_db,Pointer<CartesianGridGeometry<NDIM> > grid_geometry,
-           double wall_ghost_dist)
+#include "ibamr/Wall.h"
+#include "ibamr/namespaces.h"
+
+/////////////////////////////// NAMESPACE ////////////////////////////////////
+
+namespace IBAMR
+{
+/////////////////////////////// STATIC ///////////////////////////////////////
+
+/////////////////////////////// PUBLIC ///////////////////////////////////////
+
+Wall::Wall(Pointer<Database> wall_db, Pointer<CartesianGridGeometry<NDIM> > grid_geometry, double wall_ghost_dist)
 {
     // get geometry for the wall force box and scaling from cells to numerical postions
     const Box<NDIM> domain_box = grid_geometry->getPhysicalDomain()[0];
     const double* dx = grid_geometry->getDx();
 
     // get direction the wall is facing, dim
-    if(wall_db->keyExists("dim"))
+    if (wall_db->keyExists("dim"))
     {
         d_axis = wall_db->getInteger("dim");
     }
@@ -21,7 +60,7 @@ Wall::Wall(Pointer<Database> wall_db,Pointer<CartesianGridGeometry<NDIM> > grid_
     }
 
     // get the location of the wall
-    if(wall_db->keyExists("location"))
+    if (wall_db->keyExists("location"))
     {
         // location gives the numerical location of the wall, but is read in
         // as a cell number for box creation.  The convention is that the cell
@@ -30,7 +69,6 @@ Wall::Wall(Pointer<Database> wall_db,Pointer<CartesianGridGeometry<NDIM> > grid_
         //      if location is 1, and side = 0, the wall will be at location 2h,
         //      where h = cell width
         d_location = wall_db->getInteger("location");
-
     }
     else
     {
@@ -39,7 +77,7 @@ Wall::Wall(Pointer<Database> wall_db,Pointer<CartesianGridGeometry<NDIM> > grid_
 
     // which way is the wall 'pushing'.  0 - pushes negative direction
     //                                   1 - pushes positive direction
-    if(wall_db->keyExists("side"))
+    if (wall_db->keyExists("side"))
     {
         d_side = wall_db->getInteger("side");
     }
@@ -49,7 +87,7 @@ Wall::Wall(Pointer<Database> wall_db,Pointer<CartesianGridGeometry<NDIM> > grid_
     }
 
     // how far to search for particles to apply forces to.  This is padded by lagrangian ghost cell width.
-    if(wall_db->keyExists("force_distance"))
+    if (wall_db->keyExists("force_distance"))
     {
         d_force_distance = wall_db->getDouble("force_distance");
     }
@@ -57,9 +95,9 @@ Wall::Wall(Pointer<Database> wall_db,Pointer<CartesianGridGeometry<NDIM> > grid_
     {
         TBOX_ERROR("walls must have a force_distance parameter");
     }
-    
+
     Box<NDIM> force_box = domain_box;
-    
+
     if (d_side == 0)
     {
         force_box.upper(d_axis) = d_location + 1;
@@ -74,47 +112,41 @@ Wall::Wall(Pointer<Database> wall_db,Pointer<CartesianGridGeometry<NDIM> > grid_
     {
         TBOX_ERROR("Side must be 0 or 1");
     }
-    
-    d_location = (d_location + 1 - d_side)*dx[d_axis];
-    
+
+    d_location = (d_location + 1 - d_side) * dx[d_axis];
+
     d_force_area = force_box;
-    d_force_distance = d_force_distance*dx[d_axis];
+    d_force_distance = d_force_distance * dx[d_axis];
 
     // get parameters for wall force
     d_parameters = wall_db->getDoubleArray("parameters");
+}
 
-    
-} // Wall constructor
-
-
-Wall::Wall(const Wall& other_wall) :
-    d_axis(other_wall.d_axis),
-    d_side(other_wall.d_side),
-    d_location(other_wall.d_location),
-    d_force_distance(other_wall.d_force_distance),
-    d_parameters(other_wall.d_parameters),
-    d_force_area(other_wall.d_force_area)
+Wall::Wall(const Wall& other_wall)
+    : d_axis(other_wall.d_axis),
+      d_side(other_wall.d_side),
+      d_location(other_wall.d_location),
+      d_force_distance(other_wall.d_force_distance),
+      d_parameters(other_wall.d_parameters),
+      d_force_area(other_wall.d_force_area)
 {
-
     // this body intentionally blank
-    
-}// copy constructor
+    return;
+}
 
-Wall&
-Wall::operator=(const Wall& other_wall)
+Wall& Wall::operator=(const Wall& other_wall)
 {
-
-    d_axis = other_wall.d_axis;
-    d_side = other_wall.d_side;
-    d_location = other_wall.d_location;
-    d_force_distance = other_wall.d_force_distance;
-    d_force_area = other_wall.d_force_area;
-    d_parameters = other_wall.d_parameters;
-    
+    if (&other_wall != this)
+    {
+        d_axis = other_wall.d_axis;
+        d_side = other_wall.d_side;
+        d_location = other_wall.d_location;
+        d_force_distance = other_wall.d_force_distance;
+        d_force_area = other_wall.d_force_area;
+        d_parameters = other_wall.d_parameters;
+    }
     return *this;
-    
-} // copy assignment operator
-
+}
 
 void
 Wall::registerWallForceFcn(WallForceFcnPtr wall_force_fcn)
@@ -125,58 +157,51 @@ Wall::registerWallForceFcn(WallForceFcnPtr wall_force_fcn)
     return;
 } // registerWallForceFcn
 
-
 Box<NDIM>
-Wall::getForceArea(void)
-{ // return the force area
-
+Wall::getForceArea()
+{
     return d_force_area;
-
-} //getForceArea
+} // getForceArea
 
 int
-Wall::getAxis(void)
-{ // return the force area
-
+Wall::getAxis()
+{
     return d_axis;
-
-} //getAxis
+} // getAxis
 
 int
-Wall::getLocation(void)
-{ // return the force area
-
+Wall::getLocation()
+{
     return d_location;
-    
-} //getLocation
-
+} // getLocation
 
 double
 Wall::applyForce(double wall_distance, double /*eval_time*/)
 {
-    // apply forces from this wall.  For now time dependence is not implemented.
-
+    // apply forces from this wall.  for now time dependence is not implemented.
     int sgn = (wall_distance > 0.0) ? 1 : ((wall_distance < 0.0) ? -1 : 0);
-    if(sgn == 0)
+    if (sgn == 0)
     {
-        TBOX_ERROR ("Particle is on the wall, must be inside the domain");
+        TBOX_ERROR("Particle is on the wall, must be inside the domain");
         return 0.0;
     }
     else
     {
         // make sure we don't apply forces to particles that moved out of the
         // wall area, but haven't been regridded yet.
-        if (abs(wall_distance) < d_force_distance)
+        if (std::abs(wall_distance) < d_force_distance)
         {
-            return sgn*(d_wall_force_fcn)(abs(wall_distance),d_parameters);
+            return sgn * (d_wall_force_fcn)(std::abs(wall_distance), d_parameters);
         }
         else
         {
             return 0.0;
         }
     }
-
 } // applyForce
 
-
 /////////////////////////////////////////////////////////////////////////
+
+} // namespace IBAMR
+
+//////////////////////////////////////////////////////////////////////////////
