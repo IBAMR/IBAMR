7c833e6a18e1b7b988858fbf8941ad9b69476d86
Elijah <deleeke@gmail.com>
2016-08-18 15:00:47 -0700

    fix ex7 input file name and exclude non-working examples in runTests.sh

--- examples/IBFE/explicit/runTests.sh
+++ examples/IBFE/explicit/runTests.sh
@@ -2,20 +2,23 @@
 #set -e 
 #set -o pipefail
 
-for dir in ./*/
+testDirs=( ex0 ex1 ex3 ex5 ex6 ex7 ex8 )
+
+for i in "${testDirs[@]}"
 do
-    dir=${dir%*/}
+    dir=$i
     cd ${dir##*/}
     make gtest
     if [ -f "test2d" ];
     then
-        echo "************Running "test2d"************"
+        echo "************Running "test2d" in $i ************"
         ./test2d input2d.test --gtest_filter=*.2d
     fi
     if [ -f "test3d" ];
     then
-        echo "************Running "test3d"************"
+        echo "************Running "test3d" in $i ************"
         ./test3d input3d.test --gtest_filter=*.3d
     fi
     cd ..
 done
+
