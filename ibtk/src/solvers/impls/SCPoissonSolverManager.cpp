558c7ac0e0cde5633b5bb90aeca884d55b74cd03
Matthew G. Knepley <knepley@gmail.com>
2017-05-25 13:33:52 -0500

    Fixed Hypre guards

--- ibtk/src/solvers/impls/SCPoissonSolverManager.cpp
+++ ibtk/src/solvers/impls/SCPoissonSolverManager.cpp
@@ -176,9 +176,11 @@ SCPoissonSolverManager::SCPoissonSolverManager() : d_solver_maker_map()
     registerSolverFactoryFunction(DEFAULT_FAC_PRECONDITIONER, SCPoissonPointRelaxationFACOperator::allocate_solver);
     registerSolverFactoryFunction(POINT_RELAXATION_FAC_PRECONDITIONER,
                                   SCPoissonPointRelaxationFACOperator::allocate_solver);
-    registerSolverFactoryFunction(DEFAULT_LEVEL_SOLVER, SCPoissonHypreLevelSolver::allocate_solver);
 #ifdef PETSC_HAVE_HYPRE
+    registerSolverFactoryFunction(DEFAULT_LEVEL_SOLVER, SCPoissonHypreLevelSolver::allocate_solver);
     registerSolverFactoryFunction(HYPRE_LEVEL_SOLVER, SCPoissonHypreLevelSolver::allocate_solver);
+#else
+    registerSolverFactoryFunction(DEFAULT_LEVEL_SOLVER, SCPoissonPETScLevelSolver::allocate_solver);
 #endif
     registerSolverFactoryFunction(PETSC_LEVEL_SOLVER, SCPoissonPETScLevelSolver::allocate_solver);
     return;
