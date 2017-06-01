299ab1964e58300837afd909d3c57d274a8812fa
deleeke <deleeke@gmail.com>
2017-05-31 23:08:14 -0700

    fix libmesh.py to deal with --disable-glibcxx-debugging As discussed on libmesh-users mailing list. See archived message: https://sourceforge.net/p/libmesh/mailman/message/35859345/

--- config/IBAMR/packages/libmesh.py
+++ config/IBAMR/packages/libmesh.py
@@ -12,6 +12,11 @@ class Configure(config.package.GNUPackage):
     self.cxx       = 1
     return
 
+  def setupHelp(self, help):
+    import nargs
+    config.package.GNUPackage.setupHelp(self, help)
+    help.addArgument('LIBMESH', '-with-libmesh-method=<method>', nargs.Arg(None, 'dbg', 'User may provide which "METHOD" to compile against. Valid options: dbg, opt'))
+
   def setupDependencies(self, framework):
     config.package.GNUPackage.setupDependencies(self, framework)
     self.compilerFlags   = framework.require('config.compilerFlags', self)
@@ -22,8 +27,17 @@ class Configure(config.package.GNUPackage):
 
   def formGNUConfigureArgs(self):
     args = config.package.GNUPackage.formGNUConfigureArgs(self)
-    if self.compilerFlags.debugging:
+    if not self.argDB['download-libmesh']: #using User specified libmesh
+      if self.argDB['with-libmesh-method'].lower() == 'dbg':
+        args.append('--with-methods=dbg')
+        args.append('--disable-glibcxx-debugging')
+      elif self.argDB['with-libmesh-method'].lower() == 'opt':
+        args.append('--with-methods=opt')
+      else:
+        raise RuntimeError('Valid options for --with-libmesh-method=<string> are OPT or DBG')
+    elif self.compilerFlags.debugging:
       args.append('--with-methods=dbg')
+      args.append('--disable-glibcxx-debugging')
     else:
       args.append('--with-methods=opt')
     args.append('--disable-cxx11')
