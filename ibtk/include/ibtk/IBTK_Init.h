2a0cf1c5606faa8da96c9d58f63c7a57640dc5e6
Aaron Barrett <abarret@live.unc.edu>
2018-08-16 17:00:12 -0400

    Added initialization class.

---
+++ ibtk/include/ibtk/IBTK_Init.h
@@ -0,0 +1,78 @@
+// Filename: IBTK_Init.h
+//
+// Copyright (c) 2002-2017, Boyce Griffith
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice,
+//      this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of The University of North Carolina nor the names of
+//      its contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef included_IBTK_Init
+#define included_IBTK_Init
+
+/////////////////////////////// INCLUDES /////////////////////////////////////
+
+#include "SAMRAI_config.h"
+#include "tbox/SAMRAIManager.h"
+#include "tbox/SAMRAI_MPI.h"
+
+#include <petscsys.h>
+#ifdef LIBMESH_HAVE_PETSC
+#include "libmesh/libmesh.h"
+#endif
+
+#include "ibtk/IBTK_MPI.h"
+
+namespace IBTK
+{
+/**
+ * @brief Initialization for IBAMR programs.
+ *
+ * The IBTK_Init class handles the initializations for PETSc, LibMesh, and SAMRAI. This object should be initialized at
+ * the start of the main() function. The destruction of the object correctly closes the libraries.
+ *
+ */
+
+class IBTK_Init
+{
+    /**
+     * Constructor for IBTK_Init. Initializes libraries and sets the SAMRAI world communicator.
+     */
+    IBTK_Init(int argc, char* argv[], IBTK_MPI::comm communicator = PETSC_COMM_WORLD);
+
+    /**
+     * Destructor. Closes libraries appropriately.
+     */
+    ~IBTK_Init();
+
+private:
+#ifdef LIBMESH_HAVE_PETSC
+    LibMeshInit d_libmesh_init;
+#endif
+};
+
+} // namespace IBTK
+
+#endif
