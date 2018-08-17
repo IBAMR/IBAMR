9fb660d0b7cc5a8a3d51fe1479aa1ed4bb2762d0
nishantnangia <nishantnangia329@gmail.com>
2018-08-17 10:54:32 -0500

    Respecting register boundary conditions in surface tension computation

--- src/navier_stokes/SurfaceTensionForceFunction.cpp
+++ src/navier_stokes/SurfaceTensionForceFunction.cpp
@@ -287,11 +287,12 @@ SurfaceTensionForceFunction::setDataOnPatchHierarchy(const int data_idx,
     convertToHeaviside(d_C_idx, coarsest_ln, finest_ln, hierarchy);
 
     // Fill ghost cells
+    RobinBcCoefStrategy<NDIM>* phi_bc_coef = (d_adv_diff_solver->getPhysicalBcCoefs(phi_cc_var)).front();
     typedef HierarchyGhostCellInterpolation::InterpolationTransactionComponent InterpolationTransactionComponent;
     InterpolationTransactionComponent C_transaction(
-        d_C_idx, "CONSERVATIVE_LINEAR_REFINE", true, "CONSERVATIVE_COARSEN", "LINEAR", false, NULL);
+        d_C_idx, "CONSERVATIVE_LINEAR_REFINE", true, "CONSERVATIVE_COARSEN", "LINEAR", false, phi_bc_coef);
     InterpolationTransactionComponent phi_transaction(
-        d_phi_idx, "CONSERVATIVE_LINEAR_REFINE", true, "CONSERVATIVE_COARSEN", "LINEAR", false, NULL);
+        d_phi_idx, "CONSERVATIVE_LINEAR_REFINE", true, "CONSERVATIVE_COARSEN", "LINEAR", false, phi_bc_coef);
     Pointer<HierarchyGhostCellInterpolation> C_fill_op = new HierarchyGhostCellInterpolation();
     Pointer<HierarchyGhostCellInterpolation> phi_fill_op = new HierarchyGhostCellInterpolation();
     C_fill_op->initializeOperatorState(C_transaction, hierarchy, coarsest_ln, finest_ln);
