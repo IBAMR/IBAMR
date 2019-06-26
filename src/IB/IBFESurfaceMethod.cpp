2e6530acd646b72ee357fd77fb96d91749b0bcfc
Ebrahim Kolahdouz <me.kolahdouz@gmail.com>
2019-06-26 21:57:23 +0100

    add support for velocity derivative jumps and traction evaluation in IBFESurfaceMethod
    Co-Authored-By: David Wells <drwells@email.unc.edu>

--- src/IB/IBFESurfaceMethod.cpp
+++ src/IB/IBFESurfaceMethod.cpp
@@ -1,6 +1,9 @@
 // ---------------------------------------------------------------------
 //
 // Copyright (c) 2018 - 2019 by the IBAMR developers
+// Filename: IBFESurfaceMethod.cpp
+// Created on 19 May 2018 by Boyce Griffith
+// Copyright (c) 2002-2017, Boyce Griffith
 // All rights reserved.
 //
 // This file is part of IBAMR.
@@ -175,7 +178,7 @@ IBFESurfaceMethod::IBFESurfaceMethod(const std::string& object_name,
 {
     commonConstructor(object_name,
                       input_db,
-                      std::vector<MeshBase*>(1, mesh),
+                      std::vector<MeshBase*>(d_num_parts, mesh),
                       max_level_number,
                       register_for_restart,
                       restart_read_dirname,
@@ -712,6 +715,8 @@ IBFESurfaceMethod::interpolateVelocity(const int u_data_idx,
             X_vec = d_X_new_vecs[part];
         }
         copy_and_synch(*X_vec, *X_ghost_vec);
+
+        NumericVector<double>* WSS_vec = d_WSS_half_vecs[part];
 
         NumericVector<double>* WSS_vec = d_WSS_half_vecs[part];
 
@@ -1266,6 +1271,20 @@ IBFESurfaceMethod::interpolateVelocity(const int u_data_idx,
                         for (unsigned int d = 0; d < NDIM; ++d)
                         {
                             U_rhs_e[d](k) += U(d) * p_JxW;
+                        }
+                    }
+
+                    for (unsigned int k = 0; k < n_basis_DU_jump; ++k)
+                    {
+                        const double p_JxW = phi_DU_jump[k][qp] * JxW[qp];
+                        for (unsigned int d = 0; d < NDIM; ++d)
+                        {
+                            U_n_rhs_e[d](k) += U_n(d) * p_JxW;
+                            U_t_rhs_e[d](k) += U_t(d) * p_JxW;
+                            if (d_use_velocity_jump_conditions)
+                            {
+                                WSS_rhs_e[d](k) += WSS(d) * p_JxW;
+                            }
                         }
                     }
 
@@ -3469,6 +3488,65 @@ IBFESurfaceMethod::imposeJumpConditions(const int f_data_idx,
                                     TBOX_ERROR(d_object_name << ":  Restart file version different than class version."
                                                              << std::endl);
                                 }
+                                else
+                                {
+                                    TBOX_ERROR(d_object_name << ":  Restart file version different than class version."
+                                                             << std::endl);
+                                }
+
+                                if (side_u_boxes[axis].contains(i_s_up) && side_u_boxes[axis].contains(i_s_um))
+                                {
+                                    std::vector<libMesh::Point> ref_coords(1, xui);
+                                    fe_X->reinit(elem, &ref_coords);
+                                    fe_P_jump->reinit(elem, &ref_coords);
+                                    for (unsigned int l = 0; l < NDIM - 1; ++l)
+                                    {
+                                        interpolate(dx_dxi[l], 0, x_node, *dphi_dxi[l]);
+                                    }
+                                    if (NDIM == 2)
+                                    {
+                                        dx_dxi[1] = VectorValue<double>(0.0, 0.0, 1.0);
+                                    }
+                                    n = (dx_dxi[0].cross(dx_dxi[1])).unit();
+
+                                    bool found_same_intersection_point = false;
+
+                                    for (int shift = -1; shift <= 1; ++shift)
+                                    {
+                                        SideIndex<NDIM> i_s_prime = i_s_um;
+                                        i_s_prime(SideDim[axis][j]) += shift;
+                                        const std::vector<libMesh::Point>& candidate_coords =
+                                            intersectionSide_u_points[j][axis][i_s_prime];
+                                        const std::vector<libMesh::Point>& candidate_ref_coords =
+                                            intersectionSide_u_ref_coords[j][axis][i_s_prime];
+                                        const std::vector<VectorValue<double> >& candidate_normals =
+                                            intersectionSide_u_normals[j][axis][i_s_prime];
+
+                                        checkDoubleCountingIntersection(axis,
+                                                                        dx,
+                                                                        n,
+                                                                        xu,
+                                                                        xui,
+                                                                        i_s_um,
+                                                                        i_s_prime,
+                                                                        candidate_coords,
+                                                                        candidate_ref_coords,
+                                                                        candidate_normals,
+                                                                        found_same_intersection_point);
+                                        if (found_same_intersection_point) break;
+                                    }
+
+                                    if (!found_same_intersection_point)
+                                    {
+                                        // Evaluate the jump conditions and apply them
+                                        // to the Eulerian grid.
+
+                                        // Evaluate the jump conditions and apply them
+                                        // to the Eulerian grid.
+                                        if (side_u_boxes[SideDim[axis][j]].contains(i_s_um) &&
+                                            side_u_boxes[SideDim[axis][j]].contains(i_s_up))
+                                        {
+                                            TBOX_ASSERT(i_s_up(axis) - i_s_um(axis) == 1);
 
                                 if (side_u_boxes[axis].contains(i_s_up) && side_u_boxes[axis].contains(i_s_um))
                                 {
@@ -3654,6 +3732,7 @@ IBFESurfaceMethod::initializeCoordinates(const unsigned int part)
         }
     }
     X_coords.close();
+
     X_system.get_dof_map().enforce_constraints_exactly(X_system, &X_coords);
     copy_and_synch(X_coords, *X_system.current_local_solution);
     copy_and_synch(X_coords, X_system.get_vector("INITIAL_COORDINATES"));
@@ -3758,6 +3837,9 @@ IBFESurfaceMethod::commonConstructor(const std::string& object_name,
     // Set some default values.
     const bool use_adaptive_quadrature = true;
     const int point_density = 2.0;
+    d_wss_calc_width = 0.0;
+    d_p_calc_width = 0.0;
+    d_traction_activation_time = 0.0;
     const bool use_nodal_quadrature = false;
     const bool interp_use_consistent_mass_matrix = true;
     d_default_interp_spec = FEDataManager::InterpSpec("IB_4",
@@ -3769,6 +3851,11 @@ IBFESurfaceMethod::commonConstructor(const std::string& object_name,
                                                       use_nodal_quadrature);
     d_default_spread_spec = FEDataManager::SpreadSpec(
         "IB_4", QGAUSS, INVALID_ORDER, use_adaptive_quadrature, point_density, use_nodal_quadrature);
+    d_ghosts = 0;
+    d_use_velocity_jump_conditions = false;
+    d_use_pressure_jump_conditions = false;
+    d_use_l2_lagrange_family = false;
+    d_compute_fluid_traction = false;
 
     d_fe_family.resize(d_num_parts, INVALID_FE);
     d_fe_order.resize(d_num_parts, INVALID_ORDER);
@@ -3942,6 +4029,15 @@ IBFESurfaceMethod::getFromInput(Pointer<Database> db, bool /*is_from_restart*/)
         if (db->isDouble("p_calc_width")) d_p_calc_width = db->getDouble("p_calc_width");
         if (db->isDouble("traction_activation_time"))
             d_traction_activation_time = db->getDouble("traction_activation_time");
+        if (db->isBool("traction_interior_side")) d_traction_interior_side = db->getBool("traction_interior_side");
+    }
+    if (d_use_velocity_jump_conditions && d_use_pressure_jump_conditions)
+    {
+        if (db->isDouble("p_calc_width")) d_p_calc_width = db->getDouble("p_calc_width");
+        if (db->isDouble("wss_calc_width")) d_wss_calc_width = db->getDouble("wss_calc_width");
+        if (db->isDouble("traction_activation_time"))
+            d_traction_activation_time = db->getDouble("traction_activation_time");
+        if (db->isBool("compute_fluid_traction")) d_compute_fluid_traction = db->getBool("compute_fluid_traction");
         if (db->isBool("traction_interior_side")) d_traction_interior_side = db->getBool("traction_interior_side");
     }
     if (db->isBool("use_consistent_mass_matrix"))
