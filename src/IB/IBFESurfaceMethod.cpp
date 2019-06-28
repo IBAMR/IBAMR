e1c35ba6e3bbcae26c31c0e867b95a22a0626fb4
Boyce Griffith <boyceg@gmail.com>
2019-06-28 13:37:40 +0100

    fix error in switching to copy_and_synch

--- src/IB/IBFESurfaceMethod.cpp
+++ src/IB/IBFESurfaceMethod.cpp
@@ -2856,11 +2856,7 @@ IBFESurfaceMethod::computeFluidTraction(const double data_time, unsigned int par
     DU_jump_vec = d_DU_jump_half_vecs[part];
     for (unsigned int d = 0; d < NDIM; ++d)
     {
-        copy_and_synch(*DU_jump_vec[d], *DU_jump_vec[d]);
-    }
-    for (unsigned int d = 0; d < NDIM; ++d)
-    {
-        copy_and_synch(*DU_jump_vec[d], *DU_jump_vec[d]);
+        copy_and_synch(*DU_jump_vec[d], *DU_jump_ghost_vec[d]);
     }
 
     std::unique_ptr<NumericVector<double> > TAU_rhs_vec = TAU_vec->zero_clone();
