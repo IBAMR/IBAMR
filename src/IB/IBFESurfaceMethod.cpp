f24d56bb45686e9286cba16ba58b5655b92a85e3
Ebrahim Kolahdouz <me.kolahdouz@gmail.com>
2019-06-28 11:34:12 +0430

    so far the regular IB seems to be working

--- src/IB/IBFESurfaceMethod.cpp
+++ src/IB/IBFESurfaceMethod.cpp
@@ -1702,9 +1702,6 @@ IBFESurfaceMethod::computeLagrangianForce(const double data_time)
         std::vector<const std::vector<VectorValue<double> >*> surface_force_grad_var_data,
             surface_pressure_grad_var_data;
 
-      
-  
-
         // Loop over the elements to compute the right-hand side vector.
         boost::multi_array<double, 2> X_node, x_node;
         double DU[NDIM][NDIM];
@@ -1724,7 +1721,6 @@ IBFESurfaceMethod::computeLagrangianForce(const double data_time)
             {
                 F_rhs_e[d].resize(static_cast<int>(F_dof_indices[d].size()));
             }
-            
             if (d_use_pressure_jump_conditions)
             {
                 const auto& P_jump_dof_indices = P_jump_dof_map_cache->dof_indices(elem);
@@ -1743,8 +1739,6 @@ IBFESurfaceMethod::computeLagrangianForce(const double data_time)
                     }
                 }
             }
- 
-           
 
             fe_X->reinit(elem);
 
