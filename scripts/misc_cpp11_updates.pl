4d6bcaef65626bbbb80a732a4738d624f3dbe8d7
David Wells <drwells@email.unc.edu>
2019-11-05 11:07:05 -0500

    Fix some miscellaneous scripts

--- scripts/misc_cpp11_updates.pl
+++ scripts/misc_cpp11_updates.pl
@@ -1,4 +1,17 @@
 #! /usr/bin/perl
+## ---------------------------------------------------------------------
+##
+## Copyright (c) 2019 - 2019 by the IBAMR developers
+## All rights reserved.
+##
+## This file is part of IBAMR.
+##
+## IBAMR is free software and is distributed under the 3-clause BSD
+## license. The full text of the license can be found in the file
+## COPYRIGHT at the top level directory of IBAMR.
+##
+## ---------------------------------------------------------------------
+
 # This script does a few miscellaneous C++11 updates such as:
 #   -- replacing UniquePtr and AutoPtr with std::unique_ptr
 #   -- replacing libMesh UniquePtr header with <memory> system header
--- scripts/setup_clang_format_precommit_hook.sh
+++ scripts/setup_clang_format_precommit_hook.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
+## ---------------------------------------------------------------------
+##
+## Copyright (c) 2016 - 2016 by the IBAMR developers
+## All rights reserved.
+##
+## This file is part of IBAMR.
+##
+## IBAMR is free software and is distributed under the 3-clause BSD
+## license. The full text of the license can be found in the file
+## COPYRIGHT at the top level directory of IBAMR.
+##
+## ---------------------------------------------------------------------
+
 cp scripts/check_formatting.sh .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
