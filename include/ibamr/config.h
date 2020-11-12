d286fc39520e5bfad363a04b2f4b2a63b92ca11f
David Wells <drwells@email.unc.edu>
2020-11-12 12:50:04 -0500

    Fix some repeated macro definition warnings.

--- include/ibamr/config.h
+++ include/ibamr/config.h
@@ -19,23 +19,41 @@
 // Same as the IBTK include file - for compatibility we simply redefine all
 // macros with IBAMR
 
+// TODO: we have to guard some of these that are also defined in the old
+// IBAMR_config.h to avoid duplicated definition warnings in case a user
+// includes that header.
+
+#ifndef IBAMR_VERSION_MAJOR
 #define IBAMR_VERSION_MAJOR IBTK_VERSION_MAJOR
+#endif
 
+#ifndef IBAMR_VERSION_MINOR
 #define IBAMR_VERSION_MINOR IBTK_VERSION_MINOR
+#endif
 
+#ifndef IBAMR_VERSION_SUBMINOR
 #define IBAMR_VERSION_SUBMINOR IBTK_VERSION_SUBMINOR
+#endif
 
 #define IBAMR_VERSION_GTE(major, minor, subminor) IBTK_VERSION_GTE(major, minor, subminor)
 
 #define IBAMR_HAVE_PRAGMA_KEYWORD IBTK_HAVE_PRAGMA_KEYWORD
 
+#ifndef IBAMR_HAVE_LIBMESH
 #define IBAMR_HAVE_LIBMESH IBTK_HAVE_LIBMESH
+#endif
 
+#ifndef IBAMR_HAVE_SILO
 #define IBAMR_HAVE_SILO IBTK_HAVE_SILO
+#endif
 
+#ifndef IBAMR_FC_FUNC
 #define IBAMR_FC_FUNC(name, NAME) IBTK_FC_FUNC(name, NAME)
+#endif
 
+#ifndef IBAMR_FC_FUNC_
 #define IBAMR_FC_FUNC_(name, NAME) IBTK_FC_FUNC_(name, NAME)
+#endif
 
 #define IBAMR_DISABLE_EXTRA_WARNINGS IBTK_DISABLE_EXTRA_WARNINGS
 
