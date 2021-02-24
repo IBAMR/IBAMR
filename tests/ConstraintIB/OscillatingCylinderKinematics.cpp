c8d584ecbdda4b5b1d3e242ee3b1c2f8b191f9e7
David Wells <drwells@email.unc.edu>
2021-02-24 16:39:52 -0500

    Add a test for ConstraintIB + periodicity.
    Based on the input files that Hong Nguyen provided for #1170.

---
+++ tests/ConstraintIB/OscillatingCylinderKinematics.cpp
@@ -0,0 +1,242 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2016 - 2020 by the IBAMR developers
+// All rights reserved.
+//
+// This file is part of IBAMR.
+//
+// IBAMR is free software and is distributed under the 3-clause BSD
+// license. The full text of the license can be found in the file
+// COPYRIGHT at the top level directory of IBAMR.
+//
+// ---------------------------------------------------------------------
+
+#include "OscillatingCylinderKinematics.h"
+
+/////////////////////////////////// INCLUDES /////////////////////////////////////
+
+// SAMRAI INCLUDES
+#include "ibtk/IBTK_MPI.h"
+
+#include "CartesianPatchGeometry.h"
+#include "PatchLevel.h"
+#include "tbox/MathUtilities.h"
+#include "tbox/PIO.h"
+#include "tbox/Utilities.h"
+
+// IBAMR INCLUDES
+#include <ibamr/namespaces.h>
+
+// IBTK INCLUDES
+
+// IBTK THIRD PARTY INCLUDES
+#include "muParser.h"
+
+// C++ INCLUDES
+#include <limits>
+#include <string>
+
+namespace IBAMR
+{
+OscillatingCylinderKinematics::OscillatingCylinderKinematics(const std::string& object_name,
+                                                             Pointer<Database> input_db,
+                                                             LDataManager* l_data_manager,
+                                                             Pointer<PatchHierarchy<NDIM> > /*patch_hierarchy*/,
+                                                             bool register_for_restart)
+    : ConstraintIBKinematics(object_name, input_db, l_data_manager, register_for_restart),
+      d_prescribed_trans_vel(0.0),
+      d_current_time(0.0)
+
+{
+    // NOTE: Parent class constructor registers class with the restart manager, sets object name.
+
+    // Get kinematic parameters from the input file
+
+    if (input_db->keyExists("frequency"))
+    {
+        d_freq = input_db->getDouble("frequency");
+        pout << "FREQUENCY ================= " << d_freq << "\n\n\n";
+    }
+    else
+    {
+        TBOX_ERROR(
+            " OscillatingCylinderKinematics::OscillatingCylinderKinematics() :: frequency of cylinder does not exist "
+            "in the InputDatabase \n\n"
+            << std::endl);
+    }
+
+    if (input_db->keyExists("U_infinity"))
+    {
+        d_Uinf = input_db->getDouble("U_infinity");
+        pout << "U_INFINITY ================= " << d_Uinf << "\n\n\n";
+    }
+    else
+    {
+        TBOX_ERROR(
+            " OscillatingCylinderKinematics::OscillatingCylinderKinematics() :: maximum speed of cylinder does not "
+            "exist in the InputDatabase \n\n"
+            << std::endl);
+    }
+
+    // Set the size of vectors.
+    const StructureParameters& struct_param = getStructureParameters();
+    const int coarsest_ln = struct_param.getCoarsestLevelNumber();
+    const int finest_ln = struct_param.getFinestLevelNumber();
+    const int total_levels = finest_ln - coarsest_ln + 1;
+    const std::vector<std::pair<int, int> >& idx_range = struct_param.getLagIdxRange();
+
+    if (total_levels > 1)
+    {
+        TBOX_ERROR(
+            " OscillatingCylinderKinematics::OscillatingCylinderKinematics() :: Total levels > 1, OscillatingCylinder "
+            "should be places on only one level \n\n"
+            << std::endl);
+    }
+
+    // d_vec_coord.resize(number_lag_points);
+    // d_vec_amp.resize(number_lag_points);
+    d_new_kinematics_vel.resize(total_levels);
+    d_current_kinematics_vel.resize(total_levels);
+
+    for (int ln = 0; ln < total_levels; ++ln)
+    {
+        const int nodes_this_ln = idx_range[ln].second - idx_range[ln].first;
+        d_new_kinematics_vel[ln].resize(NDIM);
+        d_current_kinematics_vel[ln].resize(NDIM);
+
+        for (int d = 0; d < NDIM; ++d)
+        {
+            d_new_kinematics_vel[ln][d].resize(nodes_this_ln);
+            d_current_kinematics_vel[ln][d].resize(nodes_this_ln);
+        }
+    }
+
+    bool from_restart = RestartManager::getManager()->isFromRestart();
+    if (from_restart)
+    {
+        getFromRestart();
+    }
+
+    // set current velocity using current time for initial and restarted runs.
+    setOscillatingCylinderSpecificVelocity(d_current_time);
+
+    return;
+
+} // OscillatingCylinderKinematics
+
+OscillatingCylinderKinematics::~OscillatingCylinderKinematics()
+{
+    // intentionally left blank
+
+    return;
+
+} //~OscillatingCylinderKinematics
+
+void
+OscillatingCylinderKinematics::getFromRestart()
+{
+    Pointer<Database> restart_db = RestartManager::getManager()->getRootDatabase();
+    Pointer<Database> db;
+    if (restart_db->isDatabase(d_object_name))
+    {
+        db = restart_db->getDatabase(d_object_name);
+    }
+    else
+    {
+        TBOX_ERROR(d_object_name << ":  Restart database corresponding to " << d_object_name
+                                 << " not found in restart file." << std::endl);
+    }
+
+    d_current_time = db->getDouble("d_current_time");
+
+    return;
+
+} // getFromRestart
+
+void
+OscillatingCylinderKinematics::putToDatabase(Pointer<Database> db)
+{
+    IBAMR::ConstraintIBKinematics::putToDatabase(db);
+    db->putDouble("d_current_time", d_current_time);
+    return;
+
+} // putToDatabase
+
+void
+OscillatingCylinderKinematics::setOscillatingCylinderSpecificVelocity(const double time)
+{
+    // Set the size of vectors.
+    const StructureParameters& struct_param = getStructureParameters();
+    const std::vector<std::pair<int, int> >& idx_range = struct_param.getLagIdxRange();
+    const int number_lag_points = idx_range[0].second - idx_range[0].first;
+
+    for (int k = 0; k < number_lag_points; ++k)
+    {
+        d_current_kinematics_vel[0][0][k] = d_prescribed_trans_vel + d_Uinf * std::cos(2 * M_PI * d_freq * time);
+    }
+
+    return;
+
+} // setOscillatingCylinderSpecificVelocity
+
+void
+OscillatingCylinderKinematics::setKinematicsVelocity(
+    const double new_time,
+    const std::vector<double>& /*incremented_angle_from_reference_axis*/,
+    const std::vector<double>& /*center_of_mass*/,
+    const std::vector<double>& /*tagged_pt_position*/)
+{
+    d_new_time = new_time;
+    // fill current velocity at new time
+    if (!MathUtilities<double>::equalEps(0.0, d_new_time)) setOscillatingCylinderSpecificVelocity(d_new_time);
+
+    // swap current and new velocity
+    if (MathUtilities<double>::equalEps(0.0, d_new_time))
+        d_new_kinematics_vel = d_current_kinematics_vel;
+    else
+        d_new_kinematics_vel.swap(d_current_kinematics_vel);
+
+    d_current_time = d_new_time;
+
+    return;
+
+} // setNewKinematicsVelocity
+
+const std::vector<std::vector<double> >&
+OscillatingCylinderKinematics::getKinematicsVelocity(const int level) const
+{
+    static const StructureParameters& struct_param = getStructureParameters();
+    static const int coarsest_ln = struct_param.getCoarsestLevelNumber();
+
+    const int offset = level - coarsest_ln;
+    return d_new_kinematics_vel[offset];
+
+} // getNewKinematicsVelocity
+
+const std::vector<std::vector<double> >&
+OscillatingCylinderKinematics::getCurrentKinematicsVelocity(const int level) const
+{
+    static const StructureParameters& struct_param = getStructureParameters();
+    static const int coarsest_ln = struct_param.getCoarsestLevelNumber();
+
+    const int offset = level - coarsest_ln;
+    return d_current_kinematics_vel[offset];
+
+} // getCurrentKinematicsVelocity
+
+void
+OscillatingCylinderKinematics::setShape(const double /*new_time*/, const std::vector<double>&)
+{
+    // intentionally left blank
+    return;
+
+} // setNewShape
+
+const std::vector<std::vector<double> >&
+OscillatingCylinderKinematics::getShape(const int /*level*/) const
+{
+    return d_new_shape;
+
+} // getNewShape
+
+} // namespace IBAMR
---
+++ tests/ConstraintIB/oscillating_rigid_cylinder.cpp
@@ -0,0 +1,414 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2017 - 2021 by the IBAMR developers
+// All rights reserved.
+//
+// This file is part of IBAMR.
+//
+// IBAMR is free software and is distributed under the 3-clause BSD
+// license. The full text of the license can be found in the file
+// COPYRIGHT at the top level directory of IBAMR.
+//
+// ---------------------------------------------------------------------
+
+// Config files
+#include <SAMRAI_config.h>
+
+// Headers for basic PETSc functions
+#include <petscsys.h>
+
+// Headers for basic SAMRAI objects
+#include <BergerRigoutsos.h>
+#include <CartesianGridGeometry.h>
+#include <LoadBalancer.h>
+#include <StandardTagAndInitialize.h>
+
+// Headers for application-specific algorithm/data structure objects
+#include <ibamr/ConstraintIBMethod.h>
+#include <ibamr/IBExplicitHierarchyIntegrator.h>
+#include <ibamr/IBStandardForceGen.h>
+#include <ibamr/IBStandardInitializer.h>
+#include <ibamr/INSCollocatedHierarchyIntegrator.h>
+#include <ibamr/INSStaggeredHierarchyIntegrator.h>
+
+#include <ibtk/AppInitializer.h>
+#include <ibtk/IBTKInit.h>
+#include <ibtk/IBTK_MPI.h>
+#include <ibtk/LData.h>
+#include <ibtk/muParserCartGridFunction.h>
+#include <ibtk/muParserRobinBcCoefs.h>
+
+#include <ibamr/app_namespaces.h>
+
+// Application
+#include "OscillatingCylinderKinematics.h"
+
+#include "OscillatingCylinderKinematics.cpp"
+
+// Function prototypes
+void output_data(Pointer<PatchHierarchy<NDIM> > patch_hierarchy,
+                 Pointer<INSHierarchyIntegrator> navier_stokes_integrator,
+                 LDataManager* l_data_manager,
+                 const int iteration_num,
+                 const double loop_time,
+                 const string& data_dump_dirname);
+
+/*******************************************************************************
+ * For each run, the input filename and restart information (if needed) must   *
+ * be given on the command line.  For non-restarted case, command line is:     *
+ *                                                                             *
+ *    executable <input file name>                                             *
+ *                                                                             *
+ * For restarted run, command line is:                                         *
+ *                                                                             *
+ *    executable <input file name> <restart directory> <restart number>        *
+ *                                                                             *
+ *******************************************************************************/
+int
+main(int argc, char* argv[])
+{
+    // Initialize IBAMR and libraries. Deinitialization is handled by this object as well.
+    IBTKInit ibtk_init(argc, argv, MPI_COMM_WORLD);
+
+    // Several parts of the code (such as LDataManager) expect mesh files,
+    // specified in the input file, to exist in the current working
+    // directory. Rather than store them we generate them here programatically:
+    if (IBTK_MPI::getRank() == 0)
+    {
+        std::ofstream output("cylinder2d.vertex");
+        // a MATLAB version is available in examples/ConstraintIB/oscillating_rigid_cylinder/Cylinder2d.m
+
+        const double dx = 32.0 / (64 * 4 * 4 * 4);
+        const double dy = 16.0 / (32 * 4 * 4 * 4);
+
+        const double x_com = 0.0;
+        const double y_com = 0.0;
+        const double radius = 0.5;
+
+        const int num_pts_x = std::ceil(2 * radius / dx);
+        const int num_pts_y = std::ceil(2 * radius / dy);
+
+        std::vector<double> xs;
+        std::vector<double> ys;
+        for (unsigned int i = 0; i < num_pts_x; ++i)
+        {
+            const double x = x_com + i * dx - radius;
+            for (unsigned int j = 0; j < num_pts_y; ++j)
+            {
+                const double y = y_com + j * dy - radius;
+                if (std::pow(x - x_com, 2) + std::pow(y - y_com, 2) <= std::pow(radius, 2))
+                {
+                    xs.push_back(x);
+                    ys.push_back(y);
+                }
+            }
+        }
+
+        const double mean_x = std::accumulate(xs.begin(), xs.end(), 0.0) / xs.size();
+        const double mean_y = std::accumulate(ys.begin(), ys.end(), 0.0) / xs.size();
+
+        for (double& x : xs) x -= mean_x;
+
+        for (double& y : ys) y -= mean_y;
+
+        output << xs.size() << '\n';
+        output << std::fixed << std::setprecision(6);
+        for (std::size_t i = 0; i < xs.size(); ++i) output << xs[i] << "\t" << ys[i] << '\n';
+    }
+    IBTK_MPI::barrier();
+
+    { // cleanup dynamically allocated objects prior to shutdown
+
+        // Parse command line options, set some standard options from the input
+        // file, initialize the restart database (if this is a restarted run),
+        // and enable file logging.
+        Pointer<AppInitializer> app_initializer = new AppInitializer(argc, argv, "IB.log");
+        Pointer<Database> input_db = app_initializer->getInputDatabase();
+
+        // Get various standard options set in the input file.
+        const bool dump_viz_data = app_initializer->dumpVizData();
+        const int viz_dump_interval = app_initializer->getVizDumpInterval();
+        const bool uses_visit = dump_viz_data && !app_initializer->getVisItDataWriter().isNull();
+
+        const bool dump_restart_data = app_initializer->dumpRestartData();
+        const int restart_dump_interval = app_initializer->getRestartDumpInterval();
+        const string restart_dump_dirname = app_initializer->getRestartDumpDirectory();
+
+        const bool dump_postproc_data = app_initializer->dumpPostProcessingData();
+        const int postproc_data_dump_interval = app_initializer->getPostProcessingDataDumpInterval();
+        const string postproc_data_dump_dirname = app_initializer->getPostProcessingDataDumpDirectory();
+        if (dump_postproc_data && (postproc_data_dump_interval > 0) && !postproc_data_dump_dirname.empty())
+        {
+            Utilities::recursiveMkdir(postproc_data_dump_dirname);
+        }
+
+        const bool dump_timer_data = app_initializer->dumpTimerData();
+        const int timer_dump_interval = app_initializer->getTimerDumpInterval();
+
+        // Create major algorithm and data objects that comprise the
+        // application.  These objects are configured from the input database
+        // and, if this is a restarted run, from the restart database.
+        Pointer<INSHierarchyIntegrator> navier_stokes_integrator = new INSStaggeredHierarchyIntegrator(
+            "INSStaggeredHierarchyIntegrator",
+            app_initializer->getComponentDatabase("INSStaggeredHierarchyIntegrator"));
+
+        const int num_structures = input_db->getIntegerWithDefault("num_structures", 1);
+        Pointer<ConstraintIBMethod> ib_method_ops = new ConstraintIBMethod(
+            "ConstraintIBMethod", app_initializer->getComponentDatabase("ConstraintIBMethod"), num_structures);
+        Pointer<IBHierarchyIntegrator> time_integrator =
+            new IBExplicitHierarchyIntegrator("IBHierarchyIntegrator",
+                                              app_initializer->getComponentDatabase("IBHierarchyIntegrator"),
+                                              ib_method_ops,
+                                              navier_stokes_integrator);
+
+        Pointer<CartesianGridGeometry<NDIM> > grid_geometry = new CartesianGridGeometry<NDIM>(
+            "CartesianGeometry", app_initializer->getComponentDatabase("CartesianGeometry"));
+        Pointer<PatchHierarchy<NDIM> > patch_hierarchy = new PatchHierarchy<NDIM>("PatchHierarchy", grid_geometry);
+
+        Pointer<StandardTagAndInitialize<NDIM> > error_detector =
+            new StandardTagAndInitialize<NDIM>("StandardTagAndInitialize",
+                                               time_integrator,
+                                               app_initializer->getComponentDatabase("StandardTagAndInitialize"));
+        Pointer<BergerRigoutsos<NDIM> > box_generator = new BergerRigoutsos<NDIM>();
+        Pointer<LoadBalancer<NDIM> > load_balancer =
+            new LoadBalancer<NDIM>("LoadBalancer", app_initializer->getComponentDatabase("LoadBalancer"));
+        Pointer<GriddingAlgorithm<NDIM> > gridding_algorithm =
+            new GriddingAlgorithm<NDIM>("GriddingAlgorithm",
+                                        app_initializer->getComponentDatabase("GriddingAlgorithm"),
+                                        error_detector,
+                                        box_generator,
+                                        load_balancer);
+
+        // Configure the IB solver.
+        Pointer<IBStandardInitializer> ib_initializer = new IBStandardInitializer(
+            "IBStandardInitializer", app_initializer->getComponentDatabase("IBStandardInitializer"));
+        ib_method_ops->registerLInitStrategy(ib_initializer);
+        Pointer<IBStandardForceGen> ib_force_fcn = new IBStandardForceGen();
+        ib_method_ops->registerIBLagrangianForceFunction(ib_force_fcn);
+
+        // Create Eulerian initial condition specification objects.
+        if (input_db->keyExists("VelocityInitialConditions"))
+        {
+            Pointer<CartGridFunction> u_init = new muParserCartGridFunction(
+                "u_init", app_initializer->getComponentDatabase("VelocityInitialConditions"), grid_geometry);
+            navier_stokes_integrator->registerVelocityInitialConditions(u_init);
+        }
+
+        if (input_db->keyExists("PressureInitialConditions"))
+        {
+            Pointer<CartGridFunction> p_init = new muParserCartGridFunction(
+                "p_init", app_initializer->getComponentDatabase("PressureInitialConditions"), grid_geometry);
+            navier_stokes_integrator->registerPressureInitialConditions(p_init);
+        }
+
+        // Create Eulerian boundary condition specification objects (when necessary).
+        const IntVector<NDIM>& periodic_shift = grid_geometry->getPeriodicShift();
+        vector<RobinBcCoefStrategy<NDIM>*> u_bc_coefs(NDIM);
+        if (periodic_shift.min() > 0)
+        {
+            for (unsigned int d = 0; d < NDIM; ++d)
+            {
+                u_bc_coefs[d] = NULL;
+            }
+        }
+        else
+        {
+            for (unsigned int d = 0; d < NDIM; ++d)
+            {
+                const std::string bc_coefs_name = "u_bc_coefs_" + std::to_string(d);
+
+                const std::string bc_coefs_db_name = "VelocityBcCoefs_" + std::to_string(d);
+
+                u_bc_coefs[d] = new muParserRobinBcCoefs(
+                    bc_coefs_name, app_initializer->getComponentDatabase(bc_coefs_db_name), grid_geometry);
+            }
+            navier_stokes_integrator->registerPhysicalBoundaryConditions(u_bc_coefs);
+        }
+
+        // Create Eulerian body force function specification objects.
+        if (input_db->keyExists("ForcingFunction"))
+        {
+            Pointer<CartGridFunction> f_fcn = new muParserCartGridFunction(
+                "f_fcn", app_initializer->getComponentDatabase("ForcingFunction"), grid_geometry);
+            time_integrator->registerBodyForceFunction(f_fcn);
+        }
+
+        // Set up visualization plot file writers.
+        Pointer<VisItDataWriter<NDIM> > visit_data_writer = app_initializer->getVisItDataWriter();
+        Pointer<LSiloDataWriter> silo_data_writer = app_initializer->getLSiloDataWriter();
+        if (uses_visit)
+        {
+            ib_initializer->registerLSiloDataWriter(silo_data_writer);
+            ib_method_ops->registerLSiloDataWriter(silo_data_writer);
+            time_integrator->registerVisItDataWriter(visit_data_writer);
+        }
+
+        // Initialize hierarchy configuration and data on all patches.
+        time_integrator->initializePatchHierarchy(patch_hierarchy, gridding_algorithm);
+
+        // Create ConstraintIBKinematics objects
+        vector<Pointer<ConstraintIBKinematics> > ibkinematics_ops_vec;
+        Pointer<ConstraintIBKinematics> ib_kinematics_op;
+        // struct_0
+        ib_kinematics_op = new OscillatingCylinderKinematics(
+            "Cylinder",
+            app_initializer->getComponentDatabase("ConstraintIBKinematics")->getDatabase("Cylinder"),
+            ib_method_ops->getLDataManager(),
+            patch_hierarchy);
+        ibkinematics_ops_vec.push_back(ib_kinematics_op);
+
+        // register ConstraintIBKinematics objects with ConstraintIBMethod.
+        ib_method_ops->registerConstraintIBKinematics(ibkinematics_ops_vec);
+        ib_method_ops->initializeHierarchyOperatorsandData();
+
+        // Deallocate initialization objects.
+        ib_method_ops->freeLInitStrategy();
+        ib_initializer.setNull();
+        app_initializer.setNull();
+
+        // Print the input database contents to the log file.
+        plog << "Input database:\n";
+        input_db->printClassData(plog);
+
+        // Write out initial visualization data.
+        int iteration_num = time_integrator->getIntegratorStep();
+        double loop_time = time_integrator->getIntegratorTime();
+        if (dump_viz_data && uses_visit)
+        {
+            pout << "\n\nWriting visualization files...\n\n";
+            time_integrator->setupPlotData();
+            visit_data_writer->writePlotData(patch_hierarchy, iteration_num, loop_time);
+            silo_data_writer->writePlotData(iteration_num, loop_time);
+        }
+
+        std::ofstream output;
+        if (IBTK_MPI::getRank() == 0) output.open("output");
+
+        // Main time step loop.
+        double loop_time_end = time_integrator->getEndTime();
+        double dt = 0.0;
+        while (!MathUtilities<double>::equalEps(loop_time, loop_time_end) && time_integrator->stepsRemaining())
+        {
+            iteration_num = time_integrator->getIntegratorStep();
+            loop_time = time_integrator->getIntegratorTime();
+
+            pout << "\n";
+            pout << "+++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+            pout << "At beginning of timestep # " << iteration_num << "\n";
+            pout << "Simulation time is " << loop_time << "\n";
+
+            dt = time_integrator->getMaximumTimeStepSize();
+            time_integrator->advanceHierarchy(dt);
+            loop_time += dt;
+
+            pout << "\n";
+            pout << "At end       of timestep # " << iteration_num << "\n";
+            pout << "Simulation time is " << loop_time << "\n";
+            pout << "+++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+            pout << "\n";
+
+            // At specified intervals, write visualization and restart files,
+            // print out timer data, and store hierarchy data for post
+            // processing.
+            iteration_num += 1;
+            const bool last_step = !time_integrator->stepsRemaining();
+            if (dump_viz_data && uses_visit && (iteration_num % viz_dump_interval == 0 || last_step))
+            {
+                pout << "\nWriting visualization files...\n\n";
+                time_integrator->setupPlotData();
+                visit_data_writer->writePlotData(patch_hierarchy, iteration_num, loop_time);
+                silo_data_writer->writePlotData(iteration_num, loop_time);
+            }
+            if (dump_restart_data && (iteration_num % restart_dump_interval == 0 || last_step))
+            {
+                pout << "\nWriting restart files...\n\n";
+                RestartManager::getManager()->writeRestartFile(restart_dump_dirname, iteration_num);
+            }
+            if (dump_timer_data && (iteration_num % timer_dump_interval == 0 || last_step))
+            {
+                pout << "\nWriting timer data...\n\n";
+                TimerManager::getManager()->print(plog);
+            }
+            if (dump_postproc_data && (iteration_num % postproc_data_dump_interval == 0 || last_step))
+            {
+                output_data(patch_hierarchy,
+                            navier_stokes_integrator,
+                            ib_method_ops->getLDataManager(),
+                            iteration_num,
+                            loop_time,
+                            postproc_data_dump_dirname);
+            }
+        }
+
+        // Cleanup Eulerian boundary condition specification objects (when
+        // necessary).
+        for (unsigned int d = 0; d < NDIM; ++d) delete u_bc_coefs[d];
+
+    } // cleanup dynamically allocated objects prior to shutdown
+
+    // Last thing - collate the output into a single file named "output". The
+    // file names and paths are hardcoded here (this is just a test after all)
+    // but are available in the ConstraintIB input database.
+    if (IBTK_MPI::getRank() == 0)
+    {
+        const std::vector<std::string> file_names = { "Cylinder_Drag_force_struct_no_0",
+                                                      "Cylinder_COM_coordinates_struct_no_0" };
+
+        std::ofstream output("output");
+        for (const std::string file_name : file_names)
+        {
+            std::ifstream input(file_name);
+
+            output << "file name = " << file_name << '\n';
+            output << input.rdbuf();
+            output << '\n';
+        }
+    }
+} // main
+
+void
+output_data(Pointer<PatchHierarchy<NDIM> > patch_hierarchy,
+            Pointer<INSHierarchyIntegrator> navier_stokes_integrator,
+            LDataManager* l_data_manager,
+            const int iteration_num,
+            const double loop_time,
+            const string& data_dump_dirname)
+{
+    plog << "writing hierarchy data at iteration " << iteration_num << " to disk" << endl;
+    plog << "simulation time is " << loop_time << endl;
+
+    // Write Cartesian data.
+    string file_name = data_dump_dirname + "/" + "hier_data.";
+    char temp_buf[128];
+    sprintf(temp_buf, "%05d.samrai.%05d", iteration_num, IBTK_MPI::getRank());
+    file_name += temp_buf;
+    Pointer<HDFDatabase> hier_db = new HDFDatabase("hier_db");
+    hier_db->create(file_name);
+    VariableDatabase<NDIM>* var_db = VariableDatabase<NDIM>::getDatabase();
+    ComponentSelector hier_data;
+    hier_data.setFlag(var_db->mapVariableAndContextToIndex(navier_stokes_integrator->getVelocityVariable(),
+                                                           navier_stokes_integrator->getCurrentContext()));
+    hier_data.setFlag(var_db->mapVariableAndContextToIndex(navier_stokes_integrator->getPressureVariable(),
+                                                           navier_stokes_integrator->getCurrentContext()));
+    patch_hierarchy->putToDatabase(hier_db->putDatabase("PatchHierarchy"), hier_data);
+    hier_db->putDouble("loop_time", loop_time);
+    hier_db->putInteger("iteration_num", iteration_num);
+    hier_db->close();
+
+    // Write Lagrangian data.
+    const int finest_hier_level = patch_hierarchy->getFinestLevelNumber();
+    Pointer<LData> X_data = l_data_manager->getLData("X", finest_hier_level);
+    Vec X_petsc_vec = X_data->getVec();
+    Vec X_lag_vec;
+    VecDuplicate(X_petsc_vec, &X_lag_vec);
+    l_data_manager->scatterPETScToLagrangian(X_petsc_vec, X_lag_vec, finest_hier_level);
+    file_name = data_dump_dirname + "/" + "X.";
+    sprintf(temp_buf, "%05d", iteration_num);
+    file_name += temp_buf;
+    PetscViewer viewer;
+    PetscViewerASCIIOpen(PETSC_COMM_WORLD, file_name.c_str(), &viewer);
+    VecView(X_lag_vec, viewer);
+    PetscViewerDestroy(&viewer);
+    VecDestroy(&X_lag_vec);
+    return;
+} // output_data
