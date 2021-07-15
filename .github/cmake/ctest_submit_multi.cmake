6f297a7347da8b9ac86a37498d639e36b3796747
David Wells <drwells@email.unc.edu>
2021-07-15 09:54:48 -0400

    Add copyright statements.

--- .github/cmake/ctest_submit_multi.cmake
+++ .github/cmake/ctest_submit_multi.cmake
@@ -1,3 +1,16 @@
+## ---------------------------------------------------------------------
+##
+## Copyright (c) 2021 - 2021 by the IBAMR developers
+## All rights reserved.
+##
+## This file is part of IBAMR.
+##
+## IBAMR is free software and is distributed under the 3-clause BSD
+## license. The full text of the license can be found in the file
+## COPYRIGHT at the top level directory of IBAMR.
+##
+## ---------------------------------------------------------------------
+
 function (ctest_submit_multi)
   foreach (site IN LISTS drop_sites)
     foreach (variable IN ITEMS SITE METHOD LOCATION SITE_CDASH)
