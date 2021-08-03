1fb13807787ebec2017a0b94f3ab22b3ef65f634
David Wells <drwells@email.unc.edu>
2021-08-03 10:37:12 -0400

    Update copyright

--- include/ibamr/IBStandardForceGen.h
+++ include/ibamr/IBStandardForceGen.h
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------
 //
-// Copyright (c) 2007 - 2020 by the IBAMR developers
+// Copyright (c) 2007 - 2021 by the IBAMR developers
 // All rights reserved.
 //
 // This file is part of IBAMR.
--- src/navier_stokes/StaggeredStokesPETScLevelSolver.cpp
+++ src/navier_stokes/StaggeredStokesPETScLevelSolver.cpp
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------
 //
-// Copyright (c) 2014 - 2020 by the IBAMR developers
+// Copyright (c) 2014 - 2021 by the IBAMR developers
 // All rights reserved.
 //
 // This file is part of IBAMR.
