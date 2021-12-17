d462f58de89cba844a68975cf6dea45d2f799148
David Wells <drwells@email.unc.edu>
2021-12-17 11:38:01 -0500

    Clean up the first IIM example.
    - Remove support for IBFEMethod
    - Remove custom kernel support
    - Move to a separate directory
    - Fix the CFL restriction in 2D

--- examples/IIM/ex0/example.cpp
+++ examples/IIM/ex0/example.cpp
@@ -44,46 +44,6 @@
 // Set up application namespace declarations
 #include <ibamr/app_namespaces.h>
 
-inline double
-kernel(double x)
-{
-    x += 4.;
-    const double x2 = x * x;
-    const double x3 = x * x2;
-    const double x4 = x * x3;
-    const double x5 = x * x4;
-    const double x6 = x * x5;
-    const double x7 = x * x6;
-    if (x <= 0.)
-        return 0.;
-    else if (x <= 1.)
-        return .1984126984126984e-3 * x7;
-    else if (x <= 2.)
-        return .1111111111111111e-1 * x6 - .1388888888888889e-2 * x7 - .3333333333333333e-1 * x5 +
-               .5555555555555556e-1 * x4 - .5555555555555556e-1 * x3 + .3333333333333333e-1 * x2 -
-               .1111111111111111e-1 * x + .1587301587301587e-2;
-    else if (x <= 3.)
-        return .4333333333333333 * x5 - .6666666666666667e-1 * x6 + .4166666666666667e-2 * x7 - 1.500000000000000 * x4 +
-               3.055555555555556 * x3 - 3.700000000000000 * x2 + 2.477777777777778 * x - .7095238095238095;
-    else if (x <= 4.)
-        return 9. * x4 - 1.666666666666667 * x5 + .1666666666666667 * x6 - .6944444444444444e-2 * x7 -
-               28.44444444444444 * x3 + 53. * x2 - 54.22222222222222 * x + 23.59047619047619;
-    else if (x <= 5.)
-        return 96. * x3 - 22.11111111111111 * x4 + 3. * x5 - .2222222222222222 * x6 + .6944444444444444e-2 * x7 -
-               245.6666666666667 * x2 + 344. * x - 203.9650793650794;
-    else if (x <= 6.)
-        return 483.5000000000000 * x2 - 147.0555555555556 * x3 + 26.50000000000000 * x4 - 2.833333333333333 * x5 +
-               .1666666666666667 * x6 - .4166666666666667e-2 * x7 - 871.2777777777778 * x + 664.0904761904762;
-    else if (x <= 7.)
-        return 943.1222222222222 * x - 423.7000000000000 * x2 + 104.9444444444444 * x3 - 15.50000000000000 * x4 +
-               1.366666666666667 * x5 - .6666666666666667e-1 * x6 + .1388888888888889e-2 * x7 - 891.1095238095238;
-    else if (x <= 8.)
-        return 416.1015873015873 - 364.0888888888889 * x + 136.5333333333333 * x2 - 28.44444444444444 * x3 +
-               3.555555555555556 * x4 - .2666666666666667 * x5 + .1111111111111111e-1 * x6 - .1984126984126984e-3 * x7;
-    else
-        return 0.;
-} // kernel
-
 // Elasticity model data.
 namespace ModelData
 {
@@ -108,7 +68,6 @@ struct TetherData
     }
 };
 
-bool use_boundary_mesh = false;
 bool compute_fluid_traction = false;
 
 // Tether (penalty) stress function.
@@ -220,10 +179,6 @@ main(int argc, char* argv[])
         Pointer<AppInitializer> app_initializer = new AppInitializer(argc, argv, "IB.log");
         Pointer<Database> input_db = app_initializer->getInputDatabase();
 
-        // Setup user-defined kernel function.
-        LEInteractor::s_kernel_fcn = &kernel;
-        LEInteractor::s_kernel_fcn_stencil_size = 8;
-
         // Get various standard options set in the input file.
         const bool dump_viz_data = app_initializer->dumpVizData();
         const int viz_dump_interval = app_initializer->getVizDumpInterval();
@@ -310,14 +265,13 @@ main(int argc, char* argv[])
         solid_mesh.prepare_for_use();
 
         BoundaryMesh boundary_mesh(solid_mesh.comm(), solid_mesh.mesh_dimension() - 1);
-        solid_mesh.boundary_info->sync(boundary_mesh);
+        BoundaryInfo& boundary_info = solid_mesh.get_boundary_info();
+        boundary_info.sync(boundary_mesh);
         boundary_mesh.prepare_for_use();
-
-        use_boundary_mesh = input_db->getBoolWithDefault("USE_BOUNDARY_MESH", false);
 
         compute_fluid_traction = input_db->getBoolWithDefault("COMPUTE_FLUID_TRACTION", false);
 
-        Mesh& mesh = use_boundary_mesh ? boundary_mesh : solid_mesh;
+        Mesh& mesh = boundary_mesh;
 
         // Create major algorithm and data objects that comprise the
         // application. These objects are configured from the input database
@@ -342,17 +296,10 @@ main(int argc, char* argv[])
                                                    << "Valid options are: COLLOCATED, STAGGERED");
         }
         Pointer<IBStrategy> ib_ops;
-        if (use_boundary_mesh)
-            ib_ops = new IIMethod("IIMethod",
-                                  app_initializer->getComponentDatabase("IIMethod"),
-                                  &mesh,
-                                  app_initializer->getComponentDatabase("GriddingAlgorithm")->getInteger("max_levels"));
-        else
-            ib_ops =
-                new IBFEMethod("IBFEMethod",
-                               app_initializer->getComponentDatabase("IBFEMethod"),
-                               &mesh,
-                               app_initializer->getComponentDatabase("GriddingAlgorithm")->getInteger("max_levels"));
+        ib_ops = new IIMethod("IIMethod",
+                              app_initializer->getComponentDatabase("IIMethod"),
+                              &mesh,
+                              app_initializer->getComponentDatabase("GriddingAlgorithm")->getInteger("max_levels"));
         Pointer<IBHierarchyIntegrator> time_integrator =
             new IBExplicitHierarchyIntegrator("IBHierarchyIntegrator",
                                               app_initializer->getComponentDatabase("IBHierarchyIntegrator"),
@@ -381,38 +328,12 @@ main(int argc, char* argv[])
         std::vector<int> vars(NDIM);
         for (unsigned int d = 0; d < NDIM; ++d) vars[d] = d;
 
-        if (use_boundary_mesh)
-        {
-            vector<SystemData> sys_data(1, SystemData(IIMethod::VELOCITY_SYSTEM_NAME, vars));
-            Pointer<IIMethod> ibfe_ops = ib_ops;
-            ibfe_ops->initializeFEEquationSystems();
-            equation_systems = ibfe_ops->getFEDataManager()->getEquationSystems();
-            IIMethod::LagSurfaceForceFcnData surface_fcn_data(tether_force_function, sys_data, tether_data_ptr);
-            ibfe_ops->registerLagSurfaceForceFunction(surface_fcn_data);
-        }
-        else
-        {
-            vector<SystemData> sys_data(1, SystemData(IBFEMethod::VELOCITY_SYSTEM_NAME, vars));
-            Pointer<IBFEMethod> ibfe_ops = ib_ops;
-            ibfe_ops->initializeFEEquationSystems();
-            equation_systems = ibfe_ops->getFEDataManager()->getEquationSystems();
-            IBFEMethod::PK1StressFcnData PK1_stress_data(
-                PK1_stress_function, std::vector<IBTK::SystemData>(), tether_data_ptr);
-            PK1_stress_data.quad_order =
-                Utility::string_to_enum<libMesh::Order>(input_db->getStringWithDefault("PK1_QUAD_ORDER", "THIRD"));
-            ibfe_ops->registerPK1StressFunction(PK1_stress_data);
-
-            IBFEMethod::LagBodyForceFcnData body_fcn_data(tether_force_function, sys_data, tether_data_ptr);
-            ibfe_ops->registerLagBodyForceFunction(body_fcn_data);
-
-            IBFEMethod::LagSurfaceForceFcnData surface_fcn_data(tether_force_function, sys_data, tether_data_ptr);
-            ibfe_ops->registerLagSurfaceForceFunction(surface_fcn_data);
-
-            if (input_db->getBoolWithDefault("ELIMINATE_PRESSURE_JUMPS", false))
-            {
-                ibfe_ops->registerStressNormalizationPart();
-            }
-        }
+        vector<SystemData> sys_data(1, SystemData(IIMethod::VELOCITY_SYSTEM_NAME, vars));
+        Pointer<IIMethod> ibfe_ops = ib_ops;
+        ibfe_ops->initializeFEEquationSystems();
+        equation_systems = ibfe_ops->getFEDataManager()->getEquationSystems();
+        IIMethod::LagSurfaceForceFcnData surface_fcn_data(tether_force_function, sys_data, tether_data_ptr);
+        ibfe_ops->registerLagSurfaceForceFunction(surface_fcn_data);
 
         // Create Eulerian initial condition specification objects.
         if (input_db->keyExists("VelocityInitialConditions"))
@@ -470,16 +391,7 @@ main(int argc, char* argv[])
         std::unique_ptr<ExodusII_IO> exodus_io(uses_exodus ? new ExodusII_IO(mesh) : NULL);
 
         // Initialize hierarchy configuration and data on all patches.
-        if (use_boundary_mesh)
-        {
-            Pointer<IIMethod> ibfe_ops = ib_ops;
-            ibfe_ops->initializeFEData();
-        }
-        else
-        {
-            Pointer<IBFEMethod> ibfe_ops = ib_ops;
-            ibfe_ops->initializeFEData();
-        }
+        ibfe_ops->initializeFEData();
         time_integrator->initializePatchHierarchy(patch_hierarchy, gridding_algorithm);
 
         // Deallocate initialization objects.
@@ -638,16 +550,8 @@ postprocess_data(Pointer<Database> input_db,
     System* x_system;
     System* U_system;
 
-    if (use_boundary_mesh)
-    {
-        x_system = &equation_systems->get_system(IIMethod::COORDS_SYSTEM_NAME);
-        U_system = &equation_systems->get_system(IIMethod::VELOCITY_SYSTEM_NAME);
-    }
-    else
-    {
-        x_system = &equation_systems->get_system(IBFEMethod::COORDS_SYSTEM_NAME);
-        U_system = &equation_systems->get_system(IBFEMethod::VELOCITY_SYSTEM_NAME);
-    }
+    x_system = &equation_systems->get_system(IIMethod::COORDS_SYSTEM_NAME);
+    U_system = &equation_systems->get_system(IIMethod::VELOCITY_SYSTEM_NAME);
     NumericVector<double>* x_vec = x_system->solution.get();
     NumericVector<double>* x_ghost_vec = x_system->current_local_solution.get();
     x_vec->localize(*x_ghost_vec);
@@ -673,18 +577,12 @@ postprocess_data(Pointer<Database> input_db,
     std::unique_ptr<QBase> qrule = QBase::build(QGAUSS, dim, SEVENTH);
     fe->attach_quadrature_rule(qrule.get());
     const vector<double>& JxW = fe->get_JxW();
-    const vector<libMesh::Point>& q_point = fe->get_xyz();
     const vector<vector<double> >& phi = fe->get_phi();
     const vector<vector<VectorValue<double> > >& dphi = fe->get_dphi();
 
     std::unique_ptr<FEBase> fe_face(FEBase::build(dim, dof_map.variable_type(0)));
     std::unique_ptr<QBase> qrule_face = QBase::build(QGAUSS, dim - 1, SEVENTH);
     fe_face->attach_quadrature_rule(qrule_face.get());
-    const vector<double>& JxW_face = fe_face->get_JxW();
-    const vector<libMesh::Point>& q_point_face = fe_face->get_xyz();
-    const vector<libMesh::Point>& normal_face = fe_face->get_normals();
-    const vector<vector<double> >& phi_face = fe_face->get_phi();
-    const vector<vector<VectorValue<double> > >& dphi_face = fe_face->get_dphi();
 
     std::vector<double> U_qp_vec(NDIM);
     std::vector<const std::vector<double>*> var_data(1);
@@ -724,54 +622,12 @@ postprocess_data(Pointer<Database> input_db,
             {
                 U_qp_vec[d] = U(d);
             }
-            if (use_boundary_mesh)
-                tether_force_function(F, n, N, FF, x, X, elem, 0, var_data, grad_var_data, loop_time, tether_data_ptr);
-            else
-                tether_force_function(F, n, x, q_point[qp], elem, var_data, grad_var_data, loop_time, tether_data_ptr);
+            tether_force_function(F, n, N, FF, x, X, elem, 0, var_data, grad_var_data, loop_time, tether_data_ptr);
 
             for (int d = 0; d < NDIM; ++d)
             {
                 F_integral[d] += F(d) * JxW[qp];
                 if (compute_fluid_traction) T_integral[d] += TAU(d) * JxW[qp];
-            }
-        }
-        if (!use_boundary_mesh)
-        {
-            for (unsigned short int side = 0; side < elem->n_sides(); ++side)
-            {
-                if (elem->neighbor_ptr(side)) continue;
-                fe_face->reinit(elem, side);
-                const unsigned int n_qp_face = qrule_face->n_points();
-                for (unsigned int qp = 0; qp < n_qp_face; ++qp)
-                {
-                    interpolate(x, qp, x_node, phi_face);
-                    jacobian(FF, qp, x_node, dphi_face);
-                    interpolate(U, qp, U_node, phi_face);
-                    for (unsigned int d = 0; d < NDIM; ++d)
-                    {
-                        U_qp_vec[d] = U(d);
-                    }
-                    N = normal_face[qp];
-                    tensor_inverse_transpose(FF_inv_trans, FF, NDIM);
-                    n = (FF_inv_trans * N).unit();
-
-                    tether_force_function(F,
-                                          n,
-                                          N,
-                                          FF,
-                                          x,
-                                          q_point_face[qp],
-                                          elem,
-                                          side,
-                                          var_data,
-                                          grad_var_data,
-                                          loop_time,
-                                          tether_data_ptr);
-                    for (int d = 0; d < NDIM; ++d)
-                    {
-                        F_integral[d] += F(d) * JxW_face[qp];
-                    }
-                }
             }
         }
     }
