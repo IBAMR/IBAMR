5ac1c0a05ced67a1825cab32a748b1f79ecfecb5
Aaron Barrett <abarret42@gmail.com>
2022-02-14 17:45:13 -0700

    Added implementation of SnapshotCache.

--- include/ibamr/SnapshotCache.h
+++ include/ibamr/SnapshotCache.h
@@ -20,9 +20,14 @@
 
 #include <ibamr/config.h>
 
+#include "CellVariable.h"
+#include "EdgeVariable.h"
+#include "FaceVariable.h"
 #include "HierarchyDataOpsReal.h"
 #include "IntVector.h"
-#include "Variable.h"
+#include "NodeVariable.h"
+#include "PatchHierarchy.h"
+#include "SideVariable.h"
 #include "tbox/Database.h"
 #include "tbox/Pointer.h"
 
@@ -38,7 +43,11 @@ namespace IBAMR
 /*!
  * \brief Class SnapshotCache provides a method of storing snapshots. This class can also be used to determine
  * intermediate velocity fields when at a periodic steady state.
+ *
+ * This class is templated over VariableType, which should be one of the SAMRAI variable types. This currently supports
+ * double data at face centerings cell, face, side, edge, and node.
  */
+template <class VariableType>
 class SnapshotCache
 {
 public:
@@ -46,10 +55,7 @@ public:
      * The constructor for class SnapshotCache sets some default values, reads in configuration information from input
      * databases, and registers some variable/context pairs with the VariableDatabase.
      */
-    SnapshotCache(std::string object_name,
-                  SAMRAI::tbox::Pointer<SAMRAI::tbox::Database> input_db,
-                  SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM> > var,
-                  SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > patch_hierarchy);
+    SnapshotCache(std::string object_name, SAMRAI::tbox::Pointer<SAMRAI::tbox::Database> input_db);
 
     /*!
      * \brief The destructor for class SnapshotCache deallocates patch data as needed.
@@ -67,7 +73,8 @@ public:
      * are created than initially allocated. If you want to overwrite the snapshot, you should call updateSnapshot().
      * Data is not allocated in this class until this function is called.
      */
-    void setSnapshot(int u_idx, double time, SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > hierarchy);
+    void
+    setSnapshot(int u_idx, double time, SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > current_hierarchy);
 
     /*!
      * Update a snapshot. The data from this patch index is copied into an array of indices controlled by this
@@ -75,8 +82,8 @@ public:
      */
     void updateSnapshot(int u_idx,
                         double time,
-                        double tol,
-                        SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > hierarchy);
+                        SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > current_hierarchy,
+                        double tol = 1.0e-8);
 
     /*!
      * Transfer the snapshot at the time value within a given tolerance to the patch index u_idx on the supplied patch
@@ -84,13 +91,33 @@ public:
      *
      * Note this can cause a significant amount of communication if the supplied patch hierarchy has a different
      * configuration of patches than the snapshot patch hierarchy.
+     *
+     * This function does not synchronize the hierarchy (i.e. no coarsening is performed). It copies the snapshot as it
+     * was stored.
      */
+    //@{
     void getSnapshot(int u_idx,
                      double time,
-                     double tol,
-                     SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > hierarchy);
+                     SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > current_hierarchy,
+                     double tol = 1.0e-8);
+    void getSnapshot(int u_idx,
+                     double time,
+                     SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > current_hierarchy,
+                     const std::string& snapshot_refine_type,
+                     double tol = 1.0e-8);
+    //@}
 
 private:
+    /*!
+     * Fill snapshot_idx with u_idx. This assumes that both u_idx and snapshot_idx is a valid patch data index. No error
+     * checking is done with this function. Note that the snapshot hierarchy and time value in the member map will be
+     * overwritten if currently initialized.
+     */
+    void fillSnapshot(int u_idx,
+                      SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > hierarchy,
+                      int snapshot_idx,
+                      double time);
+
     /*!
      * \brief Default constructor.
      *
@@ -123,11 +150,12 @@ private:
     /*
      * Data for tracking snapshots.
      */
-    unsigned int d_max_snapshots = 0, d_num_snapshots_stored = 0;
-    std::vector<SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM> > > d_snapshot_vars;
+    unsigned int d_num_snapshots_stored = 0;
+    std::map<int, SAMRAI::tbox::Pointer<VariableType> > d_snapshot_vars;
     SAMRAI::tbox::Pointer<SAMRAI::hier::VariableContext> d_ctx;
-    std::string d_snapshot_coarsen_type = "CONSERVATIVE_COARSEN";
     std::string d_snapshot_refine_type = "CONSERVATIVE_LINEAR_REFINE";
+    SAMRAI::hier::IntVector<NDIM> d_gcw = 1;
+    unsigned int d_depth = 1;
     std::vector<int> d_snapshot_idxs;
 
     /*
@@ -139,10 +167,15 @@ private:
      * Note we store a copy of the patch hierarchy at which the snapshot is stored. As needed, we will transfer data
      * from the snapshot hierarchy to the provided hierarchy.
      */
-    std::map<int, SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > > d_hierarchy_vec;
-
-    SAMRAI::tbox::Pointer<SAMRAI::math::HierarchyDataOpsReal<NDIM, double> > d_hier_data_ops;
+    std::map<int, SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM> > > d_snapshot_hierarchies;
 };
+
+// Instantiate the viable templates
+template class SnapshotCache<SAMRAI::pdat::CellVariable<NDIM, double> >;
+template class SnapshotCache<SAMRAI::pdat::SideVariable<NDIM, double> >;
+template class SnapshotCache<SAMRAI::pdat::NodeVariable<NDIM, double> >;
+template class SnapshotCache<SAMRAI::pdat::EdgeVariable<NDIM, double> >;
+template class SnapshotCache<SAMRAI::pdat::FaceVariable<NDIM, double> >;
 } // namespace IBAMR
 
 //////////////////////////////////////////////////////////////////////////////
---
+++ src/navier_stokes/SnapshotCache.cpp
@@ -0,0 +1,249 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2022 - 2022 by the IBAMR developers
+// All rights reserved.
+//
+// This file is part of IBAMR.
+//
+// IBAMR is free software and is distributed under the 3-clause BSD
+// license. The full text of the license can be found in the file
+// COPYRIGHT at the top level directory of IBAMR.
+//
+// ---------------------------------------------------------------------
+
+/////////////////////////////// INCLUDES /////////////////////////////////////
+
+#include "ibamr/config.h"
+
+#include "ibamr/SnapshotCache.h"
+
+#include "CartesianGridGeometry.h"
+#include "RefineAlgorithm.h"
+#include "RefineOperator.h"
+#include "VariableDatabase.h"
+
+#include <algorithm>
+
+#include "ibamr/namespaces.h" // IWYU pragma: keep
+
+/////////////////////////////// NAMESPACE ////////////////////////////////////
+
+namespace IBAMR
+{
+double
+map_to_period(const double t_start, const double t_end, double time)
+{
+    const double period = t_end - t_start;
+#ifndef NDEBUG
+    // We only deal with positive period lengths.
+    TBOX_ASSERT(period > 0.0);
+#endif
+
+    while (time < t_end)
+    {
+        // Subtract period until we're less than t_end.
+        time -= period;
+    }
+    while (time > t_start)
+    {
+        // Add period until we're greater than t_start
+        time += period;
+    }
+#ifndef NDEBUG
+    TBOX_ASSERT(time <= t_end && time >= t_start);
+#endif
+    return time;
+}
+
+template <class VariableType>
+SnapshotCache<VariableType>::SnapshotCache(std::string object_name, Pointer<Database> input_db)
+    : d_object_name(std::move(object_name))
+{
+    auto var_db = VariableDatabase<NDIM>::getDatabase();
+    d_ctx = var_db->getContext(d_object_name + "::context");
+
+    if (input_db)
+    {
+        d_snapshot_refine_type = input_db->getStringWithDefault("refine_type", d_snapshot_refine_type);
+        if (input_db->keyExists("gcw")) input_db->getIntegerArray("gcw", &d_gcw[0], NDIM);
+    }
+}
+
+template <class VariableType>
+SnapshotCache<VariableType>::~SnapshotCache()
+{
+    clearSnapshots();
+}
+
+template <class VariableType>
+void
+SnapshotCache<VariableType>::clearSnapshots()
+{
+    // Clear the map.
+    d_idx_time_map.clear();
+    auto var_db = VariableDatabase<NDIM>::getDatabase();
+    // Deallocate all patch data.
+    for (size_t i = 0; i <= d_snapshot_vars.size(); ++i)
+    {
+        const Pointer<PatchHierarchy<NDIM> >& hierarchy = d_snapshot_hierarchies[i];
+        int coarsest_ln = 0;
+        int finest_ln = hierarchy->getFinestLevelNumber();
+        for (int ln = coarsest_ln; ln <= finest_ln; ++ln)
+        {
+            Pointer<PatchLevel<NDIM> > level = hierarchy->getPatchLevel(ln);
+            if (level->checkAllocated(d_snapshot_idxs[i])) level->deallocatePatchData(d_snapshot_idxs[i]);
+        }
+        // Delete variable from variable database
+        var_db->removePatchDataIndex(d_snapshot_idxs[i]);
+    }
+
+    // Clear snapshots
+    d_snapshot_hierarchies.clear();
+    d_snapshot_idxs.clear();
+    d_snapshot_vars.clear();
+    d_num_snapshots_stored = 0;
+}
+
+template <class VariableType>
+void
+SnapshotCache<VariableType>::setSnapshot(const int u_idx,
+                                         const double time,
+                                         Pointer<PatchHierarchy<NDIM> > current_hierarchy)
+{
+    ++d_num_snapshots_stored;
+    // Push back a new variable
+    Pointer<VariableType> snapshot_var =
+        new VariableType(d_object_name + "::SnapshotVar_" + std::to_string(d_num_snapshots_stored), d_depth);
+    auto var_db = VariableDatabase<NDIM>::getDatabase();
+    int snapshot_idx = var_db->registerVariableAndContext(snapshot_var, d_ctx, d_gcw);
+    d_snapshot_idxs.push_back(snapshot_idx);
+    d_snapshot_vars[d_snapshot_idxs[d_num_snapshots_stored]] = snapshot_var;
+
+    // Now fill in snapshot
+    fillSnapshot(u_idx, current_hierarchy, snapshot_idx, time);
+}
+
+template <class VariableType>
+void
+SnapshotCache<VariableType>::updateSnapshot(const int u_idx,
+                                            const double time,
+                                            Pointer<PatchHierarchy<NDIM> > current_hierarchy,
+                                            const double tol)
+{
+    // Make sure we are on a stored snapshot.
+    auto it =
+        std::find_if(d_idx_time_map.begin(),
+                     d_idx_time_map.end(),
+                     [time, tol](const std::pair<int, double>& t) -> bool { return std::abs(t.second - time) < tol; });
+    if (it == d_idx_time_map.end())
+        TBOX_ERROR("Snapshot at time: " << time << " with tolerance " << tol << " does not exist!\n");
+
+    // We have the snapshot index. Clear the current snapshot and copy the new one.
+    const std::pair<int, double>& snapshot_idx_t_pair = *it;
+    const int snapshot_idx = snapshot_idx_t_pair.first;
+    const Pointer<PatchHierarchy<NDIM> >& snapshot_hierarchy = d_snapshot_hierarchies.at(snapshot_idx);
+
+    // First clear the current snapshot
+    const int coarsest_ln = 0;
+    const int finest_ln = snapshot_hierarchy->getFinestLevelNumber();
+    for (int ln = coarsest_ln; ln <= finest_ln; ++ln)
+    {
+        Pointer<PatchLevel<NDIM> > level = snapshot_hierarchy->getPatchLevel(ln);
+        if (level->checkAllocated(snapshot_idx)) level->deallocatePatchData(snapshot_idx);
+    }
+
+    // Now fill in snapshot
+    fillSnapshot(u_idx, current_hierarchy, snapshot_idx, time);
+}
+
+template <class VariableType>
+void
+SnapshotCache<VariableType>::getSnapshot(const int u_idx,
+                                         const double time,
+                                         Pointer<PatchHierarchy<NDIM> > current_hierarchy,
+                                         const double tol)
+{
+    getSnapshot(u_idx, time, current_hierarchy, d_snapshot_refine_type, tol);
+}
+
+template <class VariableType>
+void
+SnapshotCache<VariableType>::getSnapshot(const int u_idx,
+                                         const double time,
+                                         Pointer<PatchHierarchy<NDIM> > current_hierarchy,
+                                         const std::string& snapshot_refine_type,
+                                         const double tol)
+{
+    // Make sure we are on a stored snapshot.
+    auto it =
+        std::find_if(d_idx_time_map.begin(),
+                     d_idx_time_map.end(),
+                     [time, tol](const std::pair<int, double>& t) -> bool { return std::abs(t.second - time) < tol; });
+    if (it == d_idx_time_map.end())
+        TBOX_ERROR("Snapshot at time: " << time << " with tolerance " << tol << " does not exist!\n");
+
+    // We have our snapshot index. Now copy the data to u_idx.
+    const std::pair<int, double>& snapshot_idx_t_pair = *it;
+    const int snapshot_idx = snapshot_idx_t_pair.first;
+    const Pointer<PatchHierarchy<NDIM> >& snapshot_hierarchy = d_snapshot_hierarchies.at(snapshot_idx);
+    const Pointer<VariableType>& snapshot_var = d_snapshot_vars.at(snapshot_idx);
+    // Make sure the current hierarchy and snapshot hierarchy have the same number of levels
+    TBOX_ASSERT(current_hierarchy->getFinestLevelNumber() == snapshot_hierarchy->getFinestLevelNumber());
+    // Now transfer data from the snapshot to the current hierarchy
+    int coarsest_ln = 0;
+    int finest_ln = snapshot_hierarchy->getFinestLevelNumber();
+    for (int ln = coarsest_ln; ln <= finest_ln; ++ln)
+    {
+        Pointer<PatchLevel<NDIM> > cur_level = current_hierarchy->getPatchLevel(ln);
+        Pointer<PatchLevel<NDIM> > snp_level = snapshot_hierarchy->getPatchLevel(ln);
+
+        Pointer<RefineAlgorithm<NDIM> > refine_alg = new RefineAlgorithm<NDIM>();
+        Pointer<CartesianGridGeometry<NDIM> > grid_geom = current_hierarchy->getGridGeometry();
+        Pointer<RefineOperator<NDIM> > refine_op = grid_geom->lookupRefineOperator(snapshot_var, snapshot_refine_type);
+        refine_alg->registerRefine(u_idx, snapshot_idx, snapshot_idx, refine_op);
+        Pointer<RefineSchedule<NDIM> > schedule = refine_alg->createSchedule(cur_level, snp_level);
+
+        schedule->fillData(time);
+    }
+}
+
+template <class VariableType>
+void
+SnapshotCache<VariableType>::fillSnapshot(const int u_idx,
+                                          Pointer<PatchHierarchy<NDIM> > hierarchy,
+                                          const int snapshot_idx,
+                                          const double time)
+{
+    // Create a snapshot of the patch hierarchy.
+    Pointer<PatchHierarchy<NDIM> > snapshot_hierarchy = hierarchy->makeRefinedPatchHierarchy(
+        d_object_name + "::SnapshotHierarchy_" + std::to_string(d_num_snapshots_stored),
+        1 /*ratio*/,
+        false /*register_for_restart*/);
+    // Allocate patch data and copy data to new patch hierarchy.
+    int coarsest_ln = 0;
+    int finest_ln = snapshot_hierarchy->getFinestLevelNumber();
+    for (int ln = coarsest_ln; ln <= finest_ln; ++ln)
+    {
+        Pointer<PatchLevel<NDIM> > snapshot_level = snapshot_hierarchy->getPatchLevel(ln);
+        if (!snapshot_level->checkAllocated(snapshot_idx)) snapshot_level->allocatePatchData(snapshot_idx);
+        // We've allocated data, now copy it
+        // Note these levels should cover the same space, so we shouldn't need to have a refine operator.
+        Pointer<PatchLevel<NDIM> > old_level = hierarchy->getPatchLevel(ln);
+        Pointer<RefineAlgorithm<NDIM> > refine_alg = new RefineAlgorithm<NDIM>();
+        Pointer<RefineOperator<NDIM> > refine_op = nullptr;
+        refine_alg->registerRefine(snapshot_idx, u_idx, u_idx, refine_op);
+        Pointer<RefineSchedule<NDIM> > schedule = refine_alg->createSchedule(snapshot_level, old_level);
+
+        schedule->fillData(time);
+    }
+
+    // Store the index, time, hierarchy, and variable.
+    d_idx_time_map[d_snapshot_idxs[d_num_snapshots_stored]] = time;
+    d_snapshot_hierarchies[d_snapshot_idxs[d_num_snapshots_stored]] = snapshot_hierarchy;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+} // namespace IBAMR
+
+//////////////////////////////////////////////////////////////////////////////
