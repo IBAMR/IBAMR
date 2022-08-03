25f83a83771f7859d247807b886a4c50021ebc9e
Ramakrishnan <thirumalaisamyramu@gmail.com>
2022-08-03 16:42:34 -0700

    Add phase_change/ex2

---
+++ examples/phase_change/ex2/LevelSetInitialCondition.cpp
@@ -0,0 +1,76 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2018 - 2019 by the IBAMR developers
+// All rights reserved.
+//
+// This file is part of IBAMR.
+//
+// IBAMR is free software and is distributed under the 3-clause BSD
+// license. The full text of the license can be found in the file
+// COPYRIGHT at the top level directory of IBAMR.
+//
+// ---------------------------------------------------------------------
+
+#include "LevelSetInitialCondition.h"
+
+/////////////////////////////// INCLUDES /////////////////////////////////////
+
+#include <IBAMR_config.h>
+#include <SAMRAI_config.h>
+
+// SAMRAI INCLUDES
+#include <HierarchyDataOpsManager.h>
+
+/////////////////////////////// PUBLIC ///////////////////////////////////////
+
+LevelSetInitialCondition::LevelSetInitialCondition(const std::string& object_name, const double bubble_radius)
+    : d_object_name(object_name), d_radius(bubble_radius)
+{
+    // intentionally blank
+    return;
+} // LevelSetInitialCondition
+
+bool
+LevelSetInitialCondition::isTimeDependent() const
+{
+    return true;
+} // isTimeDependent
+
+void
+LevelSetInitialCondition::setDataOnPatch(const int data_idx,
+                                         Pointer<Variable<NDIM> > /*var*/,
+                                         Pointer<Patch<NDIM> > patch,
+                                         const double /*data_time*/,
+                                         const bool initial_time,
+                                         Pointer<PatchLevel<NDIM> > /*patch_level*/)
+{
+    // Set the temperature function throughout the domain
+    if (initial_time)
+    {
+        const Box<NDIM>& patch_box = patch->getBox();
+        Pointer<CellData<NDIM, double> > D_data = patch->getPatchData(data_idx);
+
+        Pointer<CartesianPatchGeometry<NDIM> > patch_geom = patch->getPatchGeometry();
+        const double* patch_X_lower = patch_geom->getXLower();
+        const hier::Index<NDIM>& patch_lower_idx = patch_box.lower();
+        const double* const patch_dx = patch_geom->getDx();
+
+        for (Box<NDIM>::Iterator it(patch_box); it; it++)
+        {
+            CellIndex<NDIM> ci(it());
+
+            // Get physical coordinates
+            IBTK::Vector coord = IBTK::Vector::Zero();
+            for (int d = 0; d < NDIM; ++d)
+            {
+                coord[d] = patch_X_lower[d] + patch_dx[d] * (static_cast<double>(ci(d) - patch_lower_idx(d)) + 0.5);
+            }
+
+            const double distance = std::sqrt(std::pow(coord[0] - 0.5, 2.0) + std::pow(coord[1] - 0.5, 2.0));
+            (*D_data)(ci) = d_radius - distance;
+        }
+    }
+    return;
+} // setDataOnPatch
+
+//////////////////////////////////////////////////////////////////////////////
---
+++ examples/phase_change/ex2/LevelSetInitialCondition.h
@@ -0,0 +1,89 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2018 - 2018 by the IBAMR developers
+// All rights reserved.
+//
+// This file is part of IBAMR.
+//
+// IBAMR is free software and is distributed under the 3-clause BSD
+// license. The full text of the license can be found in the file
+// COPYRIGHT at the top level directory of IBAMR.
+//
+// ---------------------------------------------------------------------
+
+#ifndef included_LevelSetInitialCondition
+#define included_LevelSetInitialCondition
+
+/////////////////////////////// INCLUDES /////////////////////////////////////
+
+// IBAMR INCLUDES
+#include <ibtk/muParserCartGridFunction.h>
+
+#include <ibamr/app_namespaces.h>
+
+/////////////////////////////// CLASS DEFINITION /////////////////////////////
+
+/*!
+ * \brief Class LevelSetInitialCondition provides an initial condition for
+ * the level set function.
+ */
+class LevelSetInitialCondition : public CartGridFunction
+{
+public:
+    /*!
+     * \brief Class constructor.
+     */
+    LevelSetInitialCondition(const std::string& object_name, const double bubble_radius);
+
+    /*!
+     * \brief Empty destructor.
+     */
+    ~LevelSetInitialCondition() = default;
+
+    /*!
+     * \brief Indicates whether the concrete TemperatureInitialCondition object is
+     * time-dependent.
+     */
+    bool isTimeDependent() const override;
+
+    /*!
+     * \brief Evaluate the function on the patch interior.
+     */
+    void setDataOnPatch(const int data_idx,
+                        Pointer<SAMRAI::hier::Variable<NDIM> > var,
+                        Pointer<Patch<NDIM> > patch,
+                        const double data_time,
+                        const bool initial_time = false,
+                        Pointer<PatchLevel<NDIM> > patch_level = Pointer<PatchLevel<NDIM> >(NULL)) override;
+
+    //\}
+
+private:
+    /*!
+     * Deleted default constructor.
+     */
+    LevelSetInitialCondition() = delete;
+
+    /*!
+     * Deleted copy constructor.
+     */
+    LevelSetInitialCondition(const LevelSetInitialCondition& from) = delete;
+
+    /*!
+     * Deleted assignment operator.
+     */
+    LevelSetInitialCondition& operator=(const LevelSetInitialCondition& that) = delete;
+
+    /*!
+     * Name of this object.
+     */
+    std::string d_object_name;
+
+    /*!
+     * Radius of a bubble.
+     */
+    const double d_radius;
+};
+//////////////////////////////////////////////////////////////////////////////
+
+#endif //#ifndef included_LevelSetInitialCondition
