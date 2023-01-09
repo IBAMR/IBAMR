149a2bdff9936d7218fdd27a18871565b15a4409
Boyce Griffith <boyceg@gmail.com>
2023-01-09 15:45:49 -0500

    use smoothed L2 projection to compute jump conditions in IIMethod

--- src/IB/IIMethod.cpp
+++ src/IB/IIMethod.cpp
@@ -2752,10 +2752,8 @@ IIMethod::computeLagrangianForce(const double data_time)
         if (d_use_pressure_jump_conditions)
         {
             P_jump_rhs_vec->close();
-            d_fe_data_managers[part]->computeL2Projection(*P_jump_vec,
-                                                          *P_jump_rhs_vec,
-                                                          PRESSURE_JUMP_SYSTEM_NAME,
-                                                          d_default_interp_spec.use_consistent_mass_matrix);
+            d_fe_data_managers[part]->computeSmoothedL2Projection(
+                *P_jump_vec, *P_jump_rhs_vec, PRESSURE_JUMP_SYSTEM_NAME, d_smoothing_eps);
             P_jump_rhs_integral = SAMRAI_MPI::sumReduction(P_jump_rhs_integral);
             surface_area = SAMRAI_MPI::sumReduction(surface_area);
             if (d_normalize_pressure_jump[part]) P_jump_vec->add(-P_jump_rhs_integral / surface_area);
@@ -2766,10 +2764,8 @@ IIMethod::computeLagrangianForce(const double data_time)
             for (unsigned int d = 0; d < NDIM; ++d)
             {
                 DU_jump_rhs_vec[d]->close();
-                d_fe_data_managers[part]->computeL2Projection(*DU_jump_vec[d],
-                                                              *DU_jump_rhs_vec[d],
-                                                              VELOCITY_JUMP_SYSTEM_NAME[d],
-                                                              d_default_interp_spec.use_consistent_mass_matrix);
+                d_fe_data_managers[part]->computeSmoothedL2Projection(
+                    *DU_jump_vec[d], *DU_jump_rhs_vec[d], VELOCITY_JUMP_SYSTEM_NAME[d], d_smoothing_eps);
                 DU_jump_vec[d]->close();
             }
         }
@@ -4313,6 +4309,8 @@ IIMethod::getFromInput(Pointer<Database> db, bool /*is_from_restart*/)
     if (db->isBool("use_consistent_mass_matrix"))
         d_use_consistent_mass_matrix = db->getBool("use_consistent_mass_matrix");
     if (db->isBool("use_direct_forcing")) d_use_direct_forcing = db->getBool("use_direct_forcing");
+
+    if (db->isDouble("smoothing_eps")) d_smoothing_eps = db->getDouble("smoothing_eps");
 
     // Restart settings.
     if (db->isString("libmesh_restart_file_extension"))
