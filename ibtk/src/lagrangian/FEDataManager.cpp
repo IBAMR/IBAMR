b0e314a0937a9fd4b4f5b0ce606309bc827082ba
Boyce Griffith <boyceg@gmail.com>
2023-01-09 15:45:27 -0500

    add smoothed L2 projection to FEProjector and FEDataManager

--- ibtk/src/lagrangian/FEDataManager.cpp
+++ ibtk/src/lagrangian/FEDataManager.cpp
@@ -2419,6 +2419,26 @@ FEDataManager::computeL2Projection(NumericVector<double>& U_vec,
 } // computeL2Projection
 
 bool
+FEDataManager::computeSmoothedL2Projection(NumericVector<double>& U_vec,
+                                           NumericVector<double>& F_vec,
+                                           const std::string& system_name,
+                                           const double epsilon,
+                                           const bool close_U,
+                                           const bool close_F,
+                                           const double tol,
+                                           const unsigned int max_its)
+{
+    return d_fe_projector->computeSmoothedL2Projection(*static_cast<PetscVector<double>*>(&U_vec),
+                                                       *static_cast<PetscVector<double>*>(&F_vec),
+                                                       system_name,
+                                                       epsilon,
+                                                       close_U,
+                                                       close_F,
+                                                       tol,
+                                                       max_its);
+} // computeL2Projection
+
+bool
 FEDataManager::updateQuadratureRule(std::unique_ptr<QBase>& qrule,
                                     QuadratureType type,
                                     Order order,
