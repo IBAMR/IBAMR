a263d5c314661f442196eb0ed0335b41f3ef6504
amneetb <mail2amneet@gmail.com>
2023-07-06 11:54:01 -0700

    Phase Change Patch 6: Adding all phase change examples

---
+++ examples/phase_change/ex1/TagInterfaceRefinementCells.h
@@ -0,0 +1,63 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2019 - 2022 by the IBAMR developers
+// All rights reserved.
+//
+// This file is part of IBAMR.
+//
+// IBAMR is free software and is distributed under the 3-clause BSD
+// license. The full text of the license can be found in the file
+// COPYRIGHT at the top level directory of IBAMR.
+//
+// ---------------------------------------------------------------------
+
+/////////////////////// INCLUDE GUARD ////////////////////////////////////
+
+#ifndef included_TagInterfaceRefinementCells
+#define included_TagInterfaceRefinementCells
+
+///////////////////////////// INCLUDES ///////////////////////////////////
+namespace SAMRAI
+{
+namespace hier
+{
+template <int DIM>
+class BasePatchHierarchy;
+}
+namespace pdat
+{
+template <int DIM, class TYPE>
+class CellVariable;
+}
+} // namespace SAMRAI
+
+namespace IBAMR
+{
+class AdvDiffHierarchyIntegrator;
+}
+
+#include <tbox/DescribedClass.h>
+#include <tbox/Pointer.h>
+
+void callTagInterfaceRefinementCellsCallbackFunction(
+    SAMRAI::tbox::Pointer<SAMRAI::hier::BasePatchHierarchy<NDIM> > hierarchy,
+    const int level_number,
+    const double error_data_time,
+    const int tag_index,
+    const bool initial_time,
+    const bool uses_richardson_extrapolation_too,
+    void* ctx);
+
+struct TagInterfaceRefinementCells
+{
+    TagInterfaceRefinementCells(SAMRAI::tbox::Pointer<IBAMR::AdvDiffHierarchyIntegrator> adv_diff_solver,
+                                SAMRAI::tbox::Pointer<SAMRAI::pdat::CellVariable<NDIM, double> > scalar_var,
+                                double tag_min_val,
+                                double tag_max_val);
+    SAMRAI::tbox::Pointer<IBAMR::AdvDiffHierarchyIntegrator> d_adv_diff_solver;
+    SAMRAI::tbox::Pointer<SAMRAI::pdat::CellVariable<NDIM, double> > d_scalar_var;
+    double d_tag_min_val;
+    double d_tag_max_val;
+};
+
+#endif // #ifndef included_TagInterfaceRefinementCells
