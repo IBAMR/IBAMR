44880f11cbd47337012ebe87125e4a10309976ba
David Wells <drwells@email.unc.edu>
2025-10-24 12:51:07 -0400

    Test Array::operator= and its 'copy' ctor.

--- tests/external/array_02.cpp
+++ tests/external/array_02.cpp
@@ -78,6 +78,35 @@ test(const std::string& class_name, std::ostream& output)
         a.resizeArray(3);
         output << "  resizeArray() complete\n";
     }
+
+    // Note that this uses reference counting so we do not see any extra copies being made
+    output << "Test tbox::Array<" << class_name << ">::operator=()\n";
+    {
+        tbox::Array<T> a(4), b, c, d;
+        b = a;
+        c = a;
+        d = a;
+        TBOX_ASSERT(a.getPointer() == b.getPointer());
+        TBOX_ASSERT(a.getPointer() == c.getPointer());
+        TBOX_ASSERT(a.getPointer() == d.getPointer());
+
+        output << "  operator=() complete\n";
+    }
+
+    // Note that this uses reference counting so we do not see any extra copies being made
+    output << "Test tbox::Array<" << class_name << ">::Array(const tbox::Array<" << class_name << "> &)\n";
+    {
+        tbox::Array<T> a(4);
+        tbox::Array<T> b(a);
+        tbox::Array<T> c(b);
+        tbox::Array<T> d(c);
+
+        TBOX_ASSERT(a.getPointer() == b.getPointer());
+        TBOX_ASSERT(a.getPointer() == c.getPointer());
+        TBOX_ASSERT(a.getPointer() == d.getPointer());
+
+        output << "  'copy' constructor complete\n";
+    }
 }
 
 int
