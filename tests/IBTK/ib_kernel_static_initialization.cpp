5b3515287c3600e34f61315b3f5938487f16c9f4
Boyce Griffith <boyceg@gmail.com>
2026-09-09 18:37:11 -0400

    Add shared IB kernel specifications

---
+++ tests/IBTK/ib_kernel_static_initialization.cpp
@@ -0,0 +1,27 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2026 - 2026 by the IBAMR developers
+// All rights reserved.
+//
+// This file is part of IBAMR.
+//
+// IBAMR is free software and is distributed under the 3-clause BSD
+// license. The full text of the license can be found in the file
+// COPYRIGHT at the top level directory of IBAMR.
+//
+// ---------------------------------------------------------------------
+
+#include <ibtk/IBKernel.h>
+
+namespace
+{
+constexpr IBTK::IBKernel separate_translation_unit_kernel = IBTK::IBKernel::IB_4;
+constexpr IBTK::IBKernel separate_translation_unit_unknown = IBTK::IBKernel::UNKNOWN;
+} // namespace
+
+bool
+ib_kernel_static_initialization_valid()
+{
+    return separate_translation_unit_kernel == IBTK::IBKernel("IB_4") &&
+           separate_translation_unit_unknown == IBTK::IBKernel("unknown");
+}
