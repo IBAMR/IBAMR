9b45eb15b396ab29fa0fc290ad8511f84b0730d5
Boyce Griffith <boyceg@gmail.com>
2026-09-09 13:42:47 -0400

    Fix out-of-domain transfer indexing and boundary weights

--- tests/IBTK/index_utilities.cpp
+++ tests/IBTK/index_utilities.cpp
@@ -1,10 +1,13 @@
 #include <ibtk/AppInitializer.h>
 #include <ibtk/HierarchyMathOps.h>
 #include <ibtk/IBTKInit.h>
+#include <ibtk/IBTK_CHKERRQ.h>
 #include <ibtk/IndexUtilities.h>
 
 #include <tbox/Database.h>
 #include <tbox/Pointer.h>
+
+#include <petscao.h>
 
 #include <HierarchyDataOpsManager.h>
 
@@ -23,6 +26,64 @@ main(int argc, char** argv)
 
     IBTKInit ibtk_init(argc, argv, MPI_COMM_WORLD);
     tbox::Pointer<AppInitializer> app_initializer = new AppInitializer(argc, argv);
+
+    if (app_initializer->getInputDatabase()->getBoolWithDefault("test_mapping", false))
+    {
+        hier::Index<NDIM> lower(-3), extent(4);
+        lower(1) = 2;
+        extent(1) = 3;
+#if (NDIM == 3)
+        lower(2) = -1;
+        extent(2) = 2;
+#endif
+        const hier::Index<NDIM> upper = lower + extent - 1;
+        const int volume = extent.getProduct();
+        const int offset = 17;
+        const int depth = 2;
+        const int first = offset + depth * volume;
+        int mismatches = 0;
+        mismatches += IndexUtilities::mapIndexToInteger(lower, lower, extent, depth, offset) != first;
+        mismatches += IndexUtilities::mapIndexToInteger(upper, lower, extent, depth, offset) != first + volume - 1;
+        mismatches += IndexUtilities::mapIndexToInteger(lower - 1, lower, extent, depth, offset) != -1;
+        mismatches += IndexUtilities::mapIndexToInteger(upper + 1, lower, extent, depth, offset) != -1;
+        for (int axis = 0; axis < NDIM; ++axis)
+        {
+            hier::Index<NDIM> below = lower, above = upper;
+            --below(axis);
+            ++above(axis);
+            mismatches += IndexUtilities::mapIndexToInteger(below, lower, extent, depth, offset) != -1;
+            mismatches += IndexUtilities::mapIndexToInteger(above, lower, extent, depth, offset) != -1;
+            hier::IntVector<NDIM> periodic(0);
+            periodic(axis) = extent(axis);
+            hier::Index<NDIM> partner = below;
+            partner(axis) = upper(axis);
+            mismatches += IndexUtilities::mapIndexToInteger(below, lower, extent, depth, offset, periodic) !=
+                          IndexUtilities::mapIndexToInteger(partner, lower, extent, depth, offset);
+            partner = above;
+            partner(axis) = lower(axis);
+            mismatches += IndexUtilities::mapIndexToInteger(above, lower, extent, depth, offset, periodic) !=
+                          IndexUtilities::mapIndexToInteger(partner, lower, extent, depth, offset);
+            --below((axis + 1) % NDIM);
+            mismatches += IndexUtilities::mapIndexToInteger(below, lower, extent, depth, offset, periodic) != -1;
+        }
+        hier::IntVector<NDIM> periodic(extent);
+        mismatches +=
+            IndexUtilities::mapIndexToInteger(lower - 1, lower, extent, depth, offset, periodic) != first + volume - 1;
+        mismatches += IndexUtilities::mapIndexToInteger(upper + 1, lower, extent, depth, offset, periodic) != first;
+        PetscInt application[2] = { first, first + volume - 1 };
+        PetscInt petsc[2] = { 1, 0 };
+        AO ordering = nullptr;
+        int ierr = AOCreateMapping(PETSC_COMM_SELF, 2, application, petsc, &ordering);
+        IBTK_CHKERRQ(ierr);
+        PetscInt indices[3] = { -1, application[0], application[1] };
+        ierr = AOApplicationToPetsc(ordering, 3, indices);
+        IBTK_CHKERRQ(ierr);
+        mismatches += indices[0] != -1 || indices[1] != 1 || indices[2] != 0;
+        ierr = AODestroy(&ordering);
+        IBTK_CHKERRQ(ierr);
+        tbox::plog << "mapping mismatches = " << mismatches << '\n';
+        return mismatches ? 1 : 0;
+    }
 
     auto tuple = setup_hierarchy<NDIM>(app_initializer);
     auto patch_hierarchy = std::get<0>(tuple);
