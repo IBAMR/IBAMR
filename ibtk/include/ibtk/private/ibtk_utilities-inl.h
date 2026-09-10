383f7a5f1991e65babb59bc6392093750af01b80
Boyce Griffith <boyceg@gmail.com>
2026-09-10 13:43:52 -0400

    Stabilize regularized Heaviside phase fractions

--- ibtk/include/ibtk/private/ibtk_utilities-inl.h
+++ ibtk/include/ibtk/private/ibtk_utilities-inl.h
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------
 //
-// Copyright (c) 2026 by the IBAMR developers
+// Copyright (c) 2011 - 2026 by the IBAMR developers
 // All rights reserved.
 //
 // This file is part of IBAMR.
@@ -23,6 +23,8 @@
 #include <ibtk/IBTK_MPI.h>
 #include <ibtk/ibtk_utilities.h>
 
+#include <algorithm>
+#include <cmath>
 #include <iomanip>
 #include <sstream>
 #include <string>
@@ -31,6 +33,42 @@
 
 namespace IBTK
 {
+inline double
+smooth_heaviside(const double& phi, const double& alpha)
+{
+    if (phi <= -alpha)
+    {
+        return 0.0;
+    }
+    if (phi >= alpha)
+    {
+        return 1.0;
+    }
+    if (phi == 0.0)
+    {
+        return 0.5;
+    }
+
+    // Evaluate the smaller of H(phi) and 1 - H(phi) from the distance to the nearer
+    // cutoff. This avoids cancellation in 0.5 + 0.5*phi/alpha near phi = -alpha;
+    // the clamp keeps final rounding within [0, 1/2].
+    const double z = (alpha - std::abs(phi)) / alpha;
+    const double fraction = std::max(0.0, std::min(0.5, 0.5 * (z - std::sin(M_PI * z) / M_PI)));
+    return phi < 0.0 ? fraction : 1.0 - fraction;
+}
+
+inline double
+smooth_delta(const double& phi, const double& alpha)
+{
+    if (std::abs(phi) >= alpha)
+    {
+        return 0.0;
+    }
+    const double z = (alpha - std::abs(phi)) / alpha;
+    const double s = std::sin(0.5 * M_PI * z);
+    return (s * s) / alpha;
+}
+
 inline std::string
 format_samrai_output_filename(const int iteration_num, const std::string& data_dump_dirname, const std::string& prefix)
 {
